2d37f401ef90ce68ef1f4f22e9c2667ca24252be
Magic_Sweepy <admin>
2025-03-10 20:15:03 +0800

    perf: Change text to template code in mixin plugin.

--- src/main/kotlin/magicbook/gtlitecore/core/GTLiteMixinPlugin.kt
+++ src/main/kotlin/magicbook/gtlitecore/core/GTLiteMixinPlugin.kt
@@ -1,11 +1,12 @@
 package magicbook.gtlitecore.core
 
+import magicbook.gtlitecore.api.utils.GTLiteValues
 import zone.rong.mixinbooter.ILateMixinLoader
 
 @Suppress("unused")
 class GTLiteMixinPlugin : ILateMixinLoader
 {
 
-    override fun getMixinConfigs(): List<String> = listOf("mixins.gtlitecore.late.json")
+    override fun getMixinConfigs(): List<String> = listOf("mixins.${GTLiteValues.MODID}.late.json")
 
 }
