dd7a62d90d9a33ef0bea78a066b1cbfa90d1997f
Magic_Sweepy <admin>
2025-03-17 17:51:24 +0800

    fix: Let IBlockTier also be a Java Interface.

---
+++ src/main/kotlin/magicbook/gtlitecore/api/block/IBlockTier.java
@@ -0,0 +1,18 @@
+package magicbook.gtlitecore.api.block;
+
+import net.minecraft.util.IStringSerializable;
+
+public interface IBlockTier extends IStringSerializable
+{
+
+    default Object getInfo()
+    {
+        return null;
+    }
+
+    default Object getTier()
+    {
+        return 0;
+    }
+
+}
