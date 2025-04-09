d88cad3771bdb7abd02f9f5e46fdb2646d7bd047
Magic_Sweepy <admin>
2025-04-09 19:53:33 +0800

    Revert "feat: Generate certus quartz rod, bolt and screw for modpack GrS part."
    This reverts commit 8bc88bf54c640e417372af48d4d00c785d6f07ee.

--- src/main/kotlin/magicbook/gtlitecore/api/unification/material/GTLiteMaterialFlags.kt
+++ src/main/kotlin/magicbook/gtlitecore/api/unification/material/GTLiteMaterialFlags.kt
@@ -145,7 +145,6 @@ class GTLiteMaterialFlags
             Polytetrafluoroethylene.addFlags(GENERATE_BOLT_SCREW)
             Polybenzimidazole.addFlags(GENERATE_BOLT_SCREW)
             Zircaloy4.addFlags(GENERATE_BOLT_SCREW)
-            CertusQuartz.addFlags(GENERATE_ROD)
 
             // boule
             Diamond.addFlags(GENERATE_BOULE)
@@ -272,7 +271,6 @@ class GTLiteMaterialFlags
             // stick
             Polybenzimidazole.addFlags(GENERATE_ROD)
             Zircaloy4.addFlags(GENERATE_ROD)
-            CertusQuartz.addFlags(GENERATE_ROD)
 
             // stickLong
             Chrome.addFlags(GENERATE_LONG_ROD)
