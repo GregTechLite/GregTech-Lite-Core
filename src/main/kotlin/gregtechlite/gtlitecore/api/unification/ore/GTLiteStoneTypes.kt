f4a57f98f98993b2baba728dfa17375e30b2b5d2
Magic_Sweepy <15581235-magic_sweepy@users.noreply.gitlab.com>
2025-08-23 16:52:06 +0800

    fix: Let StoneType dropped as item dynamically.

--- src/main/kotlin/gregtechlite/gtlitecore/api/unification/ore/GTLiteStoneTypes.kt
+++ src/main/kotlin/gregtechlite/gtlitecore/api/unification/ore/GTLiteStoneTypes.kt
@@ -2,6 +2,7 @@ package gregtechlite.gtlitecore.api.unification.ore
 
 import gregtech.api.unification.material.Materials
 import gregtech.api.unification.ore.StoneType
+import gregtech.common.ConfigHolder
 import gregtechlite.gtlitecore.api.unification.GTLiteMaterials
 import gregtechlite.gtlitecore.api.unification.material.builder.StoneTypeBuilder
 import gregtechlite.gtlitecore.common.block.GTLiteBlocks
@@ -30,7 +31,6 @@ object GTLiteStoneTypes
             .material(GTLiteMaterials.Limestone)
             .state(stoneState(GTLiteStoneVariantBlock.StoneType.LIMESTONE))
             .condition { stonePredicate(it, GTLiteStoneVariantBlock.StoneType.LIMESTONE) }
-            .dropAsItem()
             .build()
 
         KOMATIITE = StoneTypeBuilder.builder(13, "komatiite")
@@ -39,7 +39,6 @@ object GTLiteStoneTypes
             .material(GTLiteMaterials.Komatiite)
             .state(stoneState(GTLiteStoneVariantBlock.StoneType.KOMATIITE))
             .condition { stonePredicate(it, GTLiteStoneVariantBlock.StoneType.KOMATIITE) }
-            .dropAsItem()
             .build()
 
         GREEN_SCHIST = StoneTypeBuilder.builder(14, "green_schist")
@@ -48,7 +47,6 @@ object GTLiteStoneTypes
             .material(GTLiteMaterials.GreenSchist)
             .state(stoneState(GTLiteStoneVariantBlock.StoneType.GREEN_SCHIST))
             .condition { stonePredicate(it, GTLiteStoneVariantBlock.StoneType.GREEN_SCHIST) }
-            .dropAsItem()
             .build()
 
         BLUE_SCHIST = StoneTypeBuilder.builder(15, "blue_schist")
@@ -57,7 +55,6 @@ object GTLiteStoneTypes
             .material(GTLiteMaterials.BlueSchist)
             .state(stoneState(GTLiteStoneVariantBlock.StoneType.BLUE_SCHIST))
             .condition { stonePredicate(it, GTLiteStoneVariantBlock.StoneType.BLUE_SCHIST) }
-            .dropAsItem()
             .build()
 
         KIMBERLITE = StoneTypeBuilder.builder(16, "kimberlite")
@@ -66,7 +63,6 @@ object GTLiteStoneTypes
             .material(GTLiteMaterials.Kimberlite)
             .state(stoneState(GTLiteStoneVariantBlock.StoneType.KIMBERLITE))
             .condition { stonePredicate(it, GTLiteStoneVariantBlock.StoneType.KIMBERLITE) }
-            .dropAsItem()
             .build()
 
         QUARTZITE = StoneTypeBuilder.builder(17, "quartzite")
@@ -75,7 +71,6 @@ object GTLiteStoneTypes
             .material(Materials.Quartzite)
             .state(stoneState(GTLiteStoneVariantBlock.StoneType.QUARTZITE))
             .condition { stonePredicate(it, GTLiteStoneVariantBlock.StoneType.QUARTZITE) }
-            .dropAsItem()
             .build()
 
         SLATE = StoneTypeBuilder.builder(18, "slate")
@@ -84,7 +79,6 @@ object GTLiteStoneTypes
             .material(GTLiteMaterials.Slate)
             .state(stoneState(GTLiteStoneVariantBlock.StoneType.SLATE))
             .condition { stonePredicate(it, GTLiteStoneVariantBlock.StoneType.SLATE) }
-            .dropAsItem()
             .build()
 
         SHALE = StoneTypeBuilder.builder(19, "shale")
@@ -93,8 +87,19 @@ object GTLiteStoneTypes
             .material(GTLiteMaterials.Shale)
             .state(stoneState(GTLiteStoneVariantBlock.StoneType.SHALE))
             .condition { stonePredicate(it, GTLiteStoneVariantBlock.StoneType.SHALE) }
-            .dropAsItem()
             .build()
+
+        if (ConfigHolder.worldgen.allUniqueStoneTypes)
+        {
+            LIMESTONE.shouldBeDroppedAsItem = true
+            KOMATIITE.shouldBeDroppedAsItem = true
+            GREEN_SCHIST.shouldBeDroppedAsItem = true
+            BLUE_SCHIST.shouldBeDroppedAsItem = true
+            KIMBERLITE.shouldBeDroppedAsItem = true
+            QUARTZITE.shouldBeDroppedAsItem = true
+            SLATE.shouldBeDroppedAsItem = true
+            SHALE.shouldBeDroppedAsItem = true
+        }
 
     }
 
