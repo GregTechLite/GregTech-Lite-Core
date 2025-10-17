292b861d2e43036569c0efc6b0c997fda95c08f7
Magic_Sweepy <Magic_Sweepy@proton.me>
2025-10-17 13:16:23 +0800

    Hide pseudo group recipe map JEI page as default.

--- src/main/kotlin/gregtechlite/gtlitecore/api/recipe/map/PseudoGroupRecipeMapBuilder.kt
+++ src/main/kotlin/gregtechlite/gtlitecore/api/recipe/map/PseudoGroupRecipeMapBuilder.kt
@@ -4,6 +4,7 @@ import gregtech.api.recipes.RecipeBuilder
 import gregtech.api.recipes.RecipeMap
 import gregtech.api.recipes.ui.RecipeMapUI
 
+@Suppress("UnstableApiUsage")
 class PseudoGroupRecipeMapBuilder<B : RecipeBuilder<B>>(private val unlocalizedName: String,
                                                         private val defaultRecipeBuilder: B)
 {
@@ -28,24 +29,39 @@ class PseudoGroupRecipeMapBuilder<B : RecipeBuilder<B>>(private val unlocalizedN
 
     fun fluidOutputs(fluidOutputs: Int) = apply { this.fluidOutputs = fluidOutputs }
 
-    fun build(): RecipeMap<B> = when (recipeMaps.size)
+    fun build(): RecipeMap<B>
     {
-        2 -> PseudoPairRecipeMap(unlocalizedName, defaultRecipeBuilder, ::buildUI,
-                                 itemInputs, itemOutputs, fluidInputs, fluidOutputs,
-                                 recipeMaps[0], recipeMaps[1])
-        3 -> PseudoTripleRecipeMap(unlocalizedName, defaultRecipeBuilder, ::buildUI,
-                                   itemInputs, itemOutputs, fluidInputs, fluidOutputs,
-                                   recipeMaps[0], recipeMaps[1], recipeMaps[2])
-        4 -> PseudoQuadrupleRecipeMap(unlocalizedName, defaultRecipeBuilder, ::buildUI,
-                                      itemInputs, itemOutputs, fluidInputs, fluidOutputs,
-                                      recipeMaps[0], recipeMaps[1], recipeMaps[2], recipeMaps[3])
-        else -> throw IllegalArgumentException("Too many recipe map which pseudo group contained")
+        var actualRecipeMap: RecipeMap<B>? = null
+        when (recipeMaps.size)
+        {
+            2 -> {
+                actualRecipeMap = PseudoPairRecipeMap(unlocalizedName, defaultRecipeBuilder, ::buildUI,
+                                                      itemInputs, itemOutputs, fluidInputs, fluidOutputs,
+                                                      recipeMaps[0], recipeMaps[1])
+            }
+            3 -> {
+                actualRecipeMap = PseudoTripleRecipeMap(unlocalizedName, defaultRecipeBuilder, ::buildUI,
+                                                        itemInputs, itemOutputs, fluidInputs, fluidOutputs,
+                                                        recipeMaps[0], recipeMaps[1], recipeMaps[2])
+            }
+            4 -> {
+                actualRecipeMap = PseudoQuadrupleRecipeMap(unlocalizedName, defaultRecipeBuilder, ::buildUI,
+                                                           itemInputs, itemOutputs, fluidInputs, fluidOutputs,
+                                                           recipeMaps[0], recipeMaps[1], recipeMaps[2], recipeMaps[3])
+            }
+            else -> null
+        }
+
+        checkNotNull(actualRecipeMap)
+
+        // Pseudo group recipe map do not generate correspondenced ui page.
+        actualRecipeMap.recipeMapUI.isJEIVisible = false
+
+        return actualRecipeMap
     }
 
-    @Suppress("UnstableApiUsage")
     private fun buildUI(recipeMap: RecipeMap<*>): RecipeMapUI<*>
     {
-        // Pseudo group recipe map do not generate correspondenced ui page.
         val ui = RecipeMapUI(recipeMap, false, false, false, false, false)
         return ui
     }
