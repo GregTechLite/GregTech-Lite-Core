2db1393e648ef129918ded6f6b84bbd43a782192
Magic_Sweepy <Magic_Sweepy@proton.me>
2025-10-31 19:49:35 +0800

    Add Gooware Mainframe AGAC recipes.

--- src/main/kotlin/gregtechlite/gtlitecore/loader/recipe/machine/AntiGravityAssemblyChamberRecipes.kt
+++ src/main/kotlin/gregtechlite/gtlitecore/loader/recipe/machine/AntiGravityAssemblyChamberRecipes.kt
@@ -4,11 +4,14 @@ import gregtech.api.GTValues.L
 import gregtech.api.GTValues.LuV
 import gregtech.api.GTValues.UHV
 import gregtech.api.GTValues.VA
+import gregtech.api.unification.material.Materials.Americium
+import gregtech.api.unification.material.Materials.Darmstadtium
 import gregtech.api.unification.material.Materials.EnrichedNaquadahTriniumEuropiumDuranide
 import gregtech.api.unification.material.Materials.Europium
 import gregtech.api.unification.material.Materials.HSSE
 import gregtech.api.unification.material.Materials.IndiumTinBariumTitaniumCuprate
 import gregtech.api.unification.material.Materials.Polybenzimidazole
+import gregtech.api.unification.material.Materials.RutheniumTriniumAmericiumNeutronate
 import gregtech.api.unification.material.Materials.SolderingAlloy
 import gregtech.api.unification.material.Materials.Tritanium
 import gregtech.api.unification.ore.OrePrefix.frameGt
@@ -19,17 +22,25 @@ import gregtech.common.items.MetaItems.CRYSTAL_MAINFRAME_UV
 import gregtech.common.items.MetaItems.WETWARE_MAINFRAME_UHV
 import gregtech.common.items.MetaItems.WETWARE_SUPER_COMPUTER_UV
 import gregtechlite.gtlitecore.api.MINUTE
+import gregtechlite.gtlitecore.api.SECOND
 import gregtechlite.gtlitecore.api.extension.EUt
-import gregtechlite.gtlitecore.api.extension.getFluid
 import gregtechlite.gtlitecore.api.recipe.GTLiteRecipeMaps.ANTI_GRAVITY_ASSEMBLY_CHAMBER_RECIPES
+import gregtechlite.gtlitecore.api.unification.GTLiteMaterials.KaptonE
+import gregtechlite.gtlitecore.api.unification.GTLiteMaterials.KaptonK
 import gregtechlite.gtlitecore.common.item.GTLiteMetaItems.GOOWARE_ASSEMBLY_UV
 import gregtechlite.gtlitecore.common.item.GTLiteMetaItems.GOOWARE_COMPUTER_UHV
+import gregtechlite.gtlitecore.common.item.GTLiteMetaItems.GOOWARE_MAINFRAME_UEV
 import gregtechlite.gtlitecore.common.item.GTLiteMetaItems.WRAP_ADVANCED_SMD_CAPACITOR
 import gregtechlite.gtlitecore.common.item.GTLiteMetaItems.WRAP_ADVANCED_SMD_DIODE
 import gregtechlite.gtlitecore.common.item.GTLiteMetaItems.WRAP_ADVANCED_SMD_INDUCTOR
 import gregtechlite.gtlitecore.common.item.GTLiteMetaItems.WRAP_ADVANCED_SMD_RESISTOR
 import gregtechlite.gtlitecore.common.item.GTLiteMetaItems.WRAP_ADVANCED_SMD_TRANSISTOR
 import gregtechlite.gtlitecore.common.item.GTLiteMetaItems.WRAP_ARAM_CHIP
+import gregtechlite.gtlitecore.common.item.GTLiteMetaItems.WRAP_COSMIC_SMD_CAPACITOR
+import gregtechlite.gtlitecore.common.item.GTLiteMetaItems.WRAP_COSMIC_SMD_DIODE
+import gregtechlite.gtlitecore.common.item.GTLiteMetaItems.WRAP_COSMIC_SMD_INDUCTOR
+import gregtechlite.gtlitecore.common.item.GTLiteMetaItems.WRAP_COSMIC_SMD_RESISTOR
+import gregtechlite.gtlitecore.common.item.GTLiteMetaItems.WRAP_COSMIC_SMD_TRANSISTOR
 import gregtechlite.gtlitecore.common.item.GTLiteMetaItems.WRAP_GOOWARE_SMD_CAPACITOR
 import gregtechlite.gtlitecore.common.item.GTLiteMetaItems.WRAP_GOOWARE_SMD_DIODE
 import gregtechlite.gtlitecore.common.item.GTLiteMetaItems.WRAP_GOOWARE_SMD_INDUCTOR
@@ -43,6 +54,11 @@ import gregtechlite.gtlitecore.common.item.GTLiteMetaItems.WRAP_OPTICAL_SMD_INDU
 import gregtechlite.gtlitecore.common.item.GTLiteMetaItems.WRAP_OPTICAL_SMD_RESISTOR
 import gregtechlite.gtlitecore.common.item.GTLiteMetaItems.WRAP_OPTICAL_SMD_TRANSISTOR
 import gregtechlite.gtlitecore.common.item.GTLiteMetaItems.WRAP_RAM_CHIP
+import gregtechlite.gtlitecore.common.item.GTLiteMetaItems.WRAP_SPINTRONIC_SMD_CAPACITOR
+import gregtechlite.gtlitecore.common.item.GTLiteMetaItems.WRAP_SPINTRONIC_SMD_DIODE
+import gregtechlite.gtlitecore.common.item.GTLiteMetaItems.WRAP_SPINTRONIC_SMD_INDUCTOR
+import gregtechlite.gtlitecore.common.item.GTLiteMetaItems.WRAP_SPINTRONIC_SMD_RESISTOR
+import gregtechlite.gtlitecore.common.item.GTLiteMetaItems.WRAP_SPINTRONIC_SMD_TRANSISTOR
 import gregtechlite.gtlitecore.common.item.GTLiteMetaItems.WRAP_ULTIMATE_CIRCUIT_BOARD
 
 object AntiGravityAssemblyChamberRecipes
@@ -324,6 +340,165 @@ object AntiGravityAssemblyChamberRecipes
             .buildAndRegister()
 
         // UEV Gooware Mainframe
+        ANTI_GRAVITY_ASSEMBLY_CHAMBER_RECIPES.recipeBuilder()
+            .circuitMeta(1)
+            .input(frameGt, Darmstadtium, 64)
+            .input(GOOWARE_COMPUTER_UHV, 48)
+            .input(WRAP_GOOWARE_SMD_DIODE, 64)
+            .input(WRAP_GOOWARE_SMD_CAPACITOR, 64)
+            .input(WRAP_GOOWARE_SMD_TRANSISTOR, 64)
+            .input(WRAP_GOOWARE_SMD_RESISTOR, 64)
+            .input(WRAP_GOOWARE_SMD_INDUCTOR, 64)
+            .input(WRAP_ARAM_CHIP, 32)
+            .input(wireGtHex, RutheniumTriniumAmericiumNeutronate, 128)
+            .fluidInputs(SolderingAlloy.getFluid(L * 40))
+            .fluidInputs(KaptonE.getFluid(L * 16))
+            .fluidInputs(KaptonK.getFluid(36 * 64 * 16)) // foil (36) * 64 * 16
+            .fluidInputs(Americium.getFluid(L * 8 * 64)) // plate (144) * 8 * 64
+            .output(GOOWARE_MAINFRAME_UEV, 16)
+            .EUt(VA[UHV])
+            .duration(20 * MINUTE) // Original: 90s, Wrapped: 90s * 16 = 1440s
+            .buildAndRegister()
+
+        ANTI_GRAVITY_ASSEMBLY_CHAMBER_RECIPES.recipeBuilder()
+            .circuitMeta(2)
+            .input(frameGt, Darmstadtium, 64 * 64)
+            .input(GOOWARE_COMPUTER_UHV, 48 * 64)
+            .input(WRAP_GOOWARE_SMD_DIODE, 64 * 64)
+            .input(WRAP_GOOWARE_SMD_CAPACITOR, 64 * 64)
+            .input(WRAP_GOOWARE_SMD_TRANSISTOR, 64 * 64)
+            .input(WRAP_GOOWARE_SMD_RESISTOR, 64 * 64)
+            .input(WRAP_GOOWARE_SMD_INDUCTOR, 64 * 64)
+            .input(WRAP_ARAM_CHIP, 32 * 64)
+            .input(wireGtHex, RutheniumTriniumAmericiumNeutronate, 128 * 64)
+            .fluidInputs(SolderingAlloy.getFluid(L * 40 * 64))
+            .fluidInputs(KaptonE.getFluid(L * 16 * 64))
+            .fluidInputs(KaptonK.getFluid(36 * 64 * 16 * 64)) // foil (36) * 64 * 16
+            .fluidInputs(Americium.getFluid(L * 8 * 64 * 64)) // plate (144) * 8 * 64
+            .output(GOOWARE_MAINFRAME_UEV, 16 * 64)
+            .EUt(VA[UHV])
+            .duration(20 * MINUTE * 64) // Original: 90s, Wrapped: 90s * 16 = 1440s
+            .buildAndRegister()
+
+        ANTI_GRAVITY_ASSEMBLY_CHAMBER_RECIPES.recipeBuilder()
+            .circuitMeta(1)
+            .input(frameGt, Darmstadtium, 64)
+            .input(GOOWARE_COMPUTER_UHV, 48)
+            .input(WRAP_OPTICAL_SMD_DIODE, 16)
+            .input(WRAP_OPTICAL_SMD_CAPACITOR, 16)
+            .input(WRAP_OPTICAL_SMD_TRANSISTOR, 16)
+            .input(WRAP_OPTICAL_SMD_RESISTOR, 16)
+            .input(WRAP_OPTICAL_SMD_INDUCTOR, 16)
+            .input(WRAP_ARAM_CHIP, 32)
+            .input(wireGtHex, RutheniumTriniumAmericiumNeutronate, 128)
+            .fluidInputs(SolderingAlloy.getFluid(L * 40))
+            .fluidInputs(KaptonE.getFluid(L * 16))
+            .fluidInputs(KaptonK.getFluid(36 * 64 * 16)) // foil (36) * 64 * 16
+            .fluidInputs(Americium.getFluid(L * 8 * 64)) // plate (144) * 8 * 64
+            .output(GOOWARE_MAINFRAME_UEV, 16)
+            .EUt(VA[UHV])
+            .duration(10 * MINUTE) // Original: 45s, Wrapped: 45s * 16 = 720s
+            .buildAndRegister()
+
+        ANTI_GRAVITY_ASSEMBLY_CHAMBER_RECIPES.recipeBuilder()
+            .circuitMeta(2)
+            .input(frameGt, Darmstadtium, 64 * 64)
+            .input(GOOWARE_COMPUTER_UHV, 48 * 64)
+            .input(WRAP_OPTICAL_SMD_DIODE, 16 * 64)
+            .input(WRAP_OPTICAL_SMD_CAPACITOR, 16 * 64)
+            .input(WRAP_OPTICAL_SMD_TRANSISTOR, 16 * 64)
+            .input(WRAP_OPTICAL_SMD_RESISTOR, 16 * 64)
+            .input(WRAP_OPTICAL_SMD_INDUCTOR, 16 * 64)
+            .input(WRAP_ARAM_CHIP, 32 * 64)
+            .input(wireGtHex, RutheniumTriniumAmericiumNeutronate, 128 * 64)
+            .fluidInputs(SolderingAlloy.getFluid(L * 40 * 64))
+            .fluidInputs(KaptonE.getFluid(L * 16 * 64))
+            .fluidInputs(KaptonK.getFluid(36 * 64 * 16 * 64)) // foil (36) * 64 * 16
+            .fluidInputs(Americium.getFluid(L * 8 * 64 * 64)) // plate (144) * 8 * 64
+            .output(GOOWARE_MAINFRAME_UEV, 16 * 64)
+            .EUt(VA[UHV])
+            .duration(10 * MINUTE * 64) // Original: 45s, Wrapped: 45s * 16 = 720s
+            .buildAndRegister()
+
+        ANTI_GRAVITY_ASSEMBLY_CHAMBER_RECIPES.recipeBuilder()
+            .circuitMeta(1)
+            .input(frameGt, Darmstadtium, 64)
+            .input(GOOWARE_COMPUTER_UHV, 48)
+            .input(WRAP_SPINTRONIC_SMD_DIODE, 4)
+            .input(WRAP_SPINTRONIC_SMD_CAPACITOR, 4)
+            .input(WRAP_SPINTRONIC_SMD_TRANSISTOR, 4)
+            .input(WRAP_SPINTRONIC_SMD_RESISTOR, 4)
+            .input(WRAP_SPINTRONIC_SMD_INDUCTOR, 4)
+            .input(WRAP_ARAM_CHIP, 32)
+            .input(wireGtHex, RutheniumTriniumAmericiumNeutronate, 128)
+            .fluidInputs(SolderingAlloy.getFluid(L * 40))
+            .fluidInputs(KaptonE.getFluid(L * 16))
+            .fluidInputs(KaptonK.getFluid(36 * 64 * 16)) // foil (36) * 64 * 16
+            .fluidInputs(Americium.getFluid(L * 8 * 64)) // plate (144) * 8 * 64
+            .output(GOOWARE_MAINFRAME_UEV, 16)
+            .EUt(VA[UHV])
+            .duration(5 * MINUTE) // Original: 22.5s, Wrapped: 22.5s * 16 = 360s
+            .buildAndRegister()
+
+        ANTI_GRAVITY_ASSEMBLY_CHAMBER_RECIPES.recipeBuilder()
+            .circuitMeta(2)
+            .input(frameGt, Darmstadtium, 64 * 64)
+            .input(GOOWARE_COMPUTER_UHV, 48 * 64)
+            .input(WRAP_SPINTRONIC_SMD_DIODE, 4 * 64)
+            .input(WRAP_SPINTRONIC_SMD_CAPACITOR, 4 * 64)
+            .input(WRAP_SPINTRONIC_SMD_TRANSISTOR, 4 * 64)
+            .input(WRAP_SPINTRONIC_SMD_RESISTOR, 4 * 64)
+            .input(WRAP_SPINTRONIC_SMD_INDUCTOR, 4 * 64)
+            .input(WRAP_ARAM_CHIP, 32 * 64)
+            .input(wireGtHex, RutheniumTriniumAmericiumNeutronate, 128 * 64)
+            .fluidInputs(SolderingAlloy.getFluid(L * 40 * 64))
+            .fluidInputs(KaptonE.getFluid(L * 16 * 64))
+            .fluidInputs(KaptonK.getFluid(36 * 64 * 16 * 64)) // foil (36) * 64 * 16
+            .fluidInputs(Americium.getFluid(L * 8 * 64 * 64)) // plate (144) * 8 * 64
+            .output(GOOWARE_MAINFRAME_UEV, 16 * 64)
+            .EUt(VA[UHV])
+            .duration(5 * MINUTE * 64) // Original: 22.5s, Wrapped: 22.5s * 16 = 360s
+            .buildAndRegister()
+
+        ANTI_GRAVITY_ASSEMBLY_CHAMBER_RECIPES.recipeBuilder()
+            .circuitMeta(1)
+            .input(frameGt, Darmstadtium, 64)
+            .input(GOOWARE_COMPUTER_UHV, 48)
+            .input(WRAP_COSMIC_SMD_DIODE)
+            .input(WRAP_COSMIC_SMD_CAPACITOR)
+            .input(WRAP_COSMIC_SMD_TRANSISTOR)
+            .input(WRAP_COSMIC_SMD_RESISTOR)
+            .input(WRAP_COSMIC_SMD_INDUCTOR)
+            .input(WRAP_ARAM_CHIP, 32)
+            .input(wireGtHex, RutheniumTriniumAmericiumNeutronate, 128)
+            .fluidInputs(SolderingAlloy.getFluid(L * 40))
+            .fluidInputs(KaptonE.getFluid(L * 16))
+            .fluidInputs(KaptonK.getFluid(36 * 64 * 16)) // foil (36) * 64 * 16
+            .fluidInputs(Americium.getFluid(L * 8 * 64)) // plate (144) * 8 * 64
+            .output(GOOWARE_MAINFRAME_UEV, 16)
+            .EUt(VA[UHV])
+            .duration(2 * MINUTE + 30 * SECOND) // Original: 11.25s, Wrapped: 1.25s * 16 = 180s
+            .buildAndRegister()
+
+        ANTI_GRAVITY_ASSEMBLY_CHAMBER_RECIPES.recipeBuilder()
+            .circuitMeta(2)
+            .input(frameGt, Darmstadtium, 64 * 64)
+            .input(GOOWARE_COMPUTER_UHV, 48 * 64)
+            .input(WRAP_COSMIC_SMD_DIODE, 64)
+            .input(WRAP_COSMIC_SMD_CAPACITOR, 64)
+            .input(WRAP_COSMIC_SMD_TRANSISTOR, 64)
+            .input(WRAP_COSMIC_SMD_RESISTOR, 64)
+            .input(WRAP_COSMIC_SMD_INDUCTOR, 64)
+            .input(WRAP_ARAM_CHIP, 32 * 64)
+            .input(wireGtHex, RutheniumTriniumAmericiumNeutronate, 128 * 64)
+            .fluidInputs(SolderingAlloy.getFluid(L * 40 * 64))
+            .fluidInputs(KaptonE.getFluid(L * 16 * 64))
+            .fluidInputs(KaptonK.getFluid(36 * 64 * 16 * 64)) // foil (36) * 64 * 16
+            .fluidInputs(Americium.getFluid(L * 8 * 64 * 64)) // plate (144) * 8 * 64
+            .output(GOOWARE_MAINFRAME_UEV, 16 * 64)
+            .EUt(VA[UHV])
+            .duration((2 * MINUTE + 30 * SECOND) * 64) // Original: 11.25s, Wrapped: 1.25s * 16 = 180s
+            .buildAndRegister()
 
     }
 
