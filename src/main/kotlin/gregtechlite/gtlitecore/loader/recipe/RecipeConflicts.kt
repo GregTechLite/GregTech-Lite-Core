1a47a05353687bd7c7fa7fca26af779836645272
Magic_Sweepy <Magic_Sweepy@proton.me>
2025-12-29 10:34:45 +0800

    Used specified int circuit for butyraldehyde recipe to resolve conflicts.

--- src/main/kotlin/gregtechlite/gtlitecore/loader/recipe/RecipeConflicts.kt
+++ src/main/kotlin/gregtechlite/gtlitecore/loader/recipe/RecipeConflicts.kt
@@ -80,7 +80,7 @@ internal object RecipeConflicts
                 CarbonMonoxide.getFluid(1000)))
 
         CHEMICAL_RECIPES.recipeBuilder()
-            .circuitMeta(2)
+            .circuitMeta(3)
             .fluidInputs(Propene.getFluid(1000))
             .fluidInputs(Hydrogen.getFluid(2000))
             .fluidInputs(CarbonMonoxide.getFluid(1000))
