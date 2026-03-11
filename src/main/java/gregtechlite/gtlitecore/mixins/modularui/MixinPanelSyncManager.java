1001944b1e19abc5b872ee3f47ba667b0a5fcbd9
Magic_Sweepy <Magic_Sweepy@proton.me>
2026-03-11 14:13:21 +0800

    slotGroup registration fixes

--- src/main/java/gregtechlite/gtlitecore/mixins/modularui/MixinPanelSyncManager.java
+++ src/main/java/gregtechlite/gtlitecore/mixins/modularui/MixinPanelSyncManager.java
@@ -3,9 +3,13 @@ package gregtechlite.gtlitecore.mixins.modularui;
 import com.cleanroommc.modularui.value.sync.ISyncRegistrar;
 import com.cleanroommc.modularui.value.sync.PanelSyncManager;
 import com.cleanroommc.modularui.value.sync.SyncHandler;
+import com.cleanroommc.modularui.widgets.slot.SlotGroup;
+import gregtechlite.gtlitecore.mixins.Implemented;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 
+@Implemented(at = "https://github.com/GregTechCEu/GregTech/pull/2900")
 @Mixin(value = PanelSyncManager.class, remap = false)
 public abstract class MixinPanelSyncManager implements ISyncRegistrar<PanelSyncManager>
 {
@@ -15,5 +19,14 @@ public abstract class MixinPanelSyncManager implements ISyncRegistrar<PanelSyncM
     {
         return syncValue(name, 0, syncHandler);
     }
+
+    @Unique
+    public PanelSyncManager registerSlotGroup(String name, int rowSize)
+    {
+        return registerSlotGroup(new SlotGroup(name, rowSize, 100, true));
+    }
+
+    @Shadow
+    public abstract PanelSyncManager registerSlotGroup(SlotGroup slotGroup);
 
 }
