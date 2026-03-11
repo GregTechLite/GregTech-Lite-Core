7cbbc834ccbced580d603f03577fe79726a744e7
Magic_Sweepy <Magic_Sweepy@proton.me>
2026-03-11 14:16:11 +0800

    add missing compat annotation for mixins class

--- src/main/java/gregtechlite/gtlitecore/mixins/modularui/MixinPanelSyncManager.java
+++ src/main/java/gregtechlite/gtlitecore/mixins/modularui/MixinPanelSyncManager.java
@@ -4,12 +4,14 @@ import com.cleanroommc.modularui.value.sync.ISyncRegistrar;
 import com.cleanroommc.modularui.value.sync.PanelSyncManager;
 import com.cleanroommc.modularui.value.sync.SyncHandler;
 import com.cleanroommc.modularui.widgets.slot.SlotGroup;
+import gregtechlite.gtlitecore.mixins.Compat;
 import gregtechlite.gtlitecore.mixins.Implemented;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 
 @Implemented(at = "https://github.com/GregTechCEu/GregTech/pull/2900")
+@Compat(modId = { "gregtech", "modularui" })
 @Mixin(value = PanelSyncManager.class, remap = false)
 public abstract class MixinPanelSyncManager implements ISyncRegistrar<PanelSyncManager>
 {
