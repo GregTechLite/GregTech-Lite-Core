50ab1c44a8ca158c7ff3e18815a39615ec0bac43
LoseUFaith <65988754+LoseUFaith@users.noreply.github.com>
2026-03-18 15:00:57 +1300

    fix text description will mix up on Space Pump Jei recipe

--- src/main/kotlin/gregtechlite/gtlitecore/integration/justenoughitems/info/SpacePumpRecipeWrapper.kt
+++ src/main/kotlin/gregtechlite/gtlitecore/integration/justenoughitems/info/SpacePumpRecipeWrapper.kt
@@ -3,6 +3,8 @@ package gregtechlite.gtlitecore.integration.justenoughitems.info
 import mezz.jei.api.ingredients.IIngredients
 import mezz.jei.api.ingredients.VanillaTypes
 import mezz.jei.api.recipe.IRecipeWrapper
+import net.minecraft.client.Minecraft
+import net.minecraft.client.resources.I18n
 import net.minecraftforge.fluids.FluidStack
 
 /**
@@ -15,6 +17,10 @@ import net.minecraftforge.fluids.FluidStack
 class SpacePumpRecipeWrapper(@JvmField val planetId: Int, @JvmField val fluidId: Int, @JvmField val fluid: FluidStack) : IRecipeWrapper
 {
 
+    private val textStartX = 5
+    private val startPosY = 40
+    private val fontHeight = Minecraft.getMinecraft().fontRenderer.FONT_HEIGHT;
+
     val fluids = ArrayList<MutableList<FluidStack>>()
 
     init
@@ -26,6 +32,32 @@ class SpacePumpRecipeWrapper(@JvmField val planetId: Int, @JvmField val fluidId:
     {
         ingredients.setInputLists(VanillaTypes.FLUID, fluids)
         ingredients.setOutputLists(VanillaTypes.FLUID, fluids)
+    }
+
+     override fun drawInfo(
+         minecraft: Minecraft,
+         recipeWidth: Int,
+         recipeHeight: Int,
+         mouseX: Int,
+         mouseY: Int
+    ) {
+        val font = minecraft.fontRenderer
+         font.getWordWrappedHeight(fluid.localizedName, recipeWidth)
+
+        gregtech.api.util.GTStringUtils.drawCenteredStringWithCutoff(
+            fluid.localizedName,
+            font,
+            recipeWidth
+        )
+
+        val planetIdName = I18n.format("gtlitecore.jei.space_pump_module.planet_id", planetId)
+        font.drawString(planetIdName, textStartX, startPosY, 0x111111)
+
+        val fluidIdName = I18n.format("gtlitecore.jei.space_pump_module.fluid_id", fluidId)
+        font.drawString(fluidIdName, textStartX, startPosY + fontHeight + 1, 0x111111)
+
+        val fluidAmountName = I18n.format("gtlitecore.jei.space_pump_module.fluid_amount", fluid.amount)
+        font.drawString(fluidAmountName, textStartX, startPosY + fontHeight * 2 + 1, 0x111111)
     }
 
 }
