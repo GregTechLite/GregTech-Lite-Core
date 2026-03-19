41edea4c29a238074cdf90119d98e4de61321c11
Magic_Sweepy <Magic_Sweepy@proton.me>
2026-03-19 11:45:19 +0800

    use wrap operation replace of overwrite

--- src/main/java/gregtechlite/gtlitecore/mixins/gregtech/MixinMultiblockUIBuilder.java
+++ src/main/java/gregtechlite/gtlitecore/mixins/gregtech/MixinMultiblockUIBuilder.java
@@ -1,151 +1,52 @@
 package gregtechlite.gtlitecore.mixins.gregtech;
 
-import com.cleanroommc.modularui.api.drawable.IDrawable;
-import com.cleanroommc.modularui.utils.serialization.ByteBufAdapters;
-import gregtech.api.capability.impl.AbstractRecipeLogic;
-import gregtech.api.metatileentity.MetaTileEntity;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Local;
 import gregtech.api.metatileentity.multiblock.ui.MultiblockUIBuilder;
-import gregtech.api.metatileentity.multiblock.ui.Operation;
-import gregtech.api.mui.GTByteBufAdapters;
-import gregtech.api.recipes.Recipe;
-import gregtech.api.recipes.RecipeMap;
-import gregtech.api.recipes.chance.output.impl.ChancedFluidOutput;
-import gregtech.api.recipes.chance.output.impl.ChancedItemOutput;
-import gregtech.api.util.GTHashMaps;
-import gregtech.api.util.GTUtility;
-import gregtech.api.util.KeyUtil;
 import gregtechlite.gtlitecore.mixins.Implemented;
-import it.unimi.dsi.fastutil.objects.Object2IntMap;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.text.TextFormatting;
 import net.minecraftforge.fluids.FluidStack;
-import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
-import org.spongepowered.asm.mixin.Shadow;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.function.Function;
+import org.spongepowered.asm.mixin.injection.At;
 
 @Implemented(at = "https://github.com/GregTechCEu/GregTech/pull/2913")
 @Mixin(value = MultiblockUIBuilder.class, remap = false)
-public abstract class MixinMultiblockUIBuilder {
+public abstract class MixinMultiblockUIBuilder
+{
 
     /**
-     * @author Magic_Sweepy
-     * @reason Fix incorrect parallel fluid output for the multiblock ui.
+     * Adds fluid output display with parallel number count.
+     * <p>
+     * This method captures the local parameter {@code p} (the parallel number from the ui syncer) and add it to fluid
+     * output line method call, now the fluid output display consistent with the rest of the output display logic by
+     * scaling it with the performed parallel count as well, i.e.
+     * <pre>{@code
+     *     // Original:
+     *     addFluidOutputLine(stack, fluidMap.getInt(stack), maxProgress);
+     *     // Now:
+     *     addFluidOutputLine(stack, (long) fluidMap.getInt(stack) * p, maxProgress);
+     * }</pre>
+     *
+     * @param instance     The target class which be mixined.
+     * @param stack        A {@link FluidStack} to display on the ui of the machine.
+     * @param count        The count of the {@code stack}.
+     * @param recipeLength The recipe length of the current recipe (on ticks).
+     * @param methodOp     The original method {@code addFluidOutputLine} call in {@code addRecipeOutputLine} method.
+     * @param p            The parallel number of the machine from the {@code InternalSyncer}.
      */
-    @Overwrite
-    public MultiblockUIBuilder addRecipeOutputLine(AbstractRecipeLogic recipeLogic, int maxLines)
+    @WrapOperation(method = "addRecipeOutputLine(Lgregtech/api/capability/impl/AbstractRecipeLogic;I)Lgregtech/api/metatileentity/multiblock/ui/MultiblockUIBuilder;",
+                   at = @At(value = "INVOKE",
+                            target = "Lgregtech/api/metatileentity/multiblock/ui/MultiblockUIBuilder;addFluidOutputLine(Lnet/minecraftforge/fluids/FluidStack;JI)V"),
+                   remap = false)
+    private void makeFluidOutputParallel(MultiblockUIBuilder instance,
+                                         FluidStack stack,
+                                         long count,
+                                         int recipeLength,
+                                         Operation<Void> methodOp,
+                                         @Local(index = 6) int p)
     {
-        Recipe recipe = recipeLogic.getPreviousRecipe();
-
-        if (getSyncer().syncBoolean(recipe == null))
-            return (MultiblockUIBuilder) (Object) this;
-
-        RecipeMap<?> recipeMap = recipeLogic.getRecipeMap();
-
-        if (getSyncer().syncBoolean(recipeMap == null))
-            return (MultiblockUIBuilder) (Object) this;
-
-        Recipe trimmedRecipe = null;
-        if (isServer())
-        {
-            MetaTileEntity mte = recipeLogic.getMetaTileEntity();
-            trimmedRecipe = Recipe.trimRecipeOutputs(recipe, recipeMap,
-                    mte.getItemOutputLimit(), mte.getFluidOutputLimit());
-        }
-
-        int p = getSyncer().syncInt(recipeLogic.getParallelRecipesPerformed());
-        if (p == 0) p = 1;
-
-        long eut = getSyncer().syncLong(trimmedRecipe == null ? 0 : trimmedRecipe.getEUt());
-        long maxVoltage = getSyncer().syncLong(recipeLogic.getMaximumOverclockVoltage());
-        int maxProgress = getSyncer().syncInt(recipeLogic.getMaxProgress());
-
-        if (maxProgress == 0) return (MultiblockUIBuilder) (Object) this;
-
-        List<ItemStack> itemOutputs = new ArrayList<>();
-        List<ChancedItemOutput> chancedItemOutputs = new ArrayList<>();
-        List<FluidStack> fluidOutputs = new ArrayList<>();
-        List<ChancedFluidOutput> chancedFluidOutputs = new ArrayList<>();
-
-        if (isServer())
-        {
-            itemOutputs.addAll(trimmedRecipe.getOutputs());
-            chancedItemOutputs.addAll(trimmedRecipe.getChancedOutputs().getChancedEntries());
-            fluidOutputs.addAll(trimmedRecipe.getFluidOutputs());
-            chancedFluidOutputs.addAll(trimmedRecipe.getChancedFluidOutputs().getChancedEntries());
-        }
-
-        itemOutputs = getSyncer().syncCollection(itemOutputs, ByteBufAdapters.ITEM_STACK);
-        fluidOutputs = getSyncer().syncCollection(fluidOutputs, ByteBufAdapters.FLUID_STACK);
-        chancedItemOutputs = getSyncer().syncCollection(chancedItemOutputs, GTByteBufAdapters.CHANCED_ITEM_OUTPUT);
-        chancedFluidOutputs = getSyncer().syncCollection(chancedFluidOutputs, GTByteBufAdapters.CHANCED_FLUID_OUTPUT);
-
-        addKey(KeyUtil.lang(TextFormatting.GRAY, "gregtech.gui.multiblock.recipe_producing"), Operation::addLine);
-
-        int recipeTier = GTUtility.getTierByVoltage(eut);
-        int machineTier = GTUtility.getOCTierByVoltage(maxVoltage);
-
-        // items
-        Object2IntMap<ItemStack> itemMap = GTHashMaps.fromItemStackCollection(itemOutputs);
-
-        for (ItemStack stack : itemMap.keySet())
-        {
-            addItemOutputLine(stack, (long) itemMap.getInt(stack) * p, maxProgress);
-        }
-
-        for (ChancedItemOutput chancedItemOutput : chancedItemOutputs)
-        {
-            // noinspection DataFlowIssue
-            int chance = getSyncer()
-                    .syncInt(() -> recipeMap.chanceFunction.getBoostedChance(chancedItemOutput, recipeTier, machineTier));
-            int count = chancedItemOutput.getIngredient().getCount() * p;
-            addChancedItemOutputLine(chancedItemOutput, count, chance, maxProgress);
-        }
-
-        // fluids
-
-        Object2IntMap<FluidStack> fluidMap = GTHashMaps.fromFluidCollection(fluidOutputs);
-
-        for (FluidStack stack : fluidMap.keySet())
-        {
-            addFluidOutputLine(stack, (long) fluidMap.getInt(stack) * p, maxProgress);
-        }
-
-        for (ChancedFluidOutput chancedFluidOutput : chancedFluidOutputs)
-        {
-            // noinspection DataFlowIssue
-            int chance = getSyncer()
-                    .syncInt(() -> recipeMap.chanceFunction.getBoostedChance(chancedFluidOutput, recipeTier, machineTier));
-            int count = chancedFluidOutput.getIngredient().amount * p;
-            addChancedFluidOutputLine(chancedFluidOutput, count, chance, maxProgress);
-        }
-        return (MultiblockUIBuilder) (Object) this;
+        long newCount = count * p;
+        methodOp.call(instance, stack, newCount, recipeLength);
     }
-
-    @Shadow
-    @NotNull
-    abstract MultiblockUIBuilder.@NotNull InternalSyncer getSyncer();
-
-    @Shadow
-    protected abstract boolean isServer();
-
-    @Shadow
-    protected abstract void addKey(IDrawable key, Function<IDrawable, Operation> function);
-
-    @Shadow
-    protected abstract void addItemOutputLine(@NotNull ItemStack stack, long count, int recipeLength);
-
-    @Shadow
-    protected abstract void addChancedItemOutputLine(@NotNull ChancedItemOutput output, int count, int chance, int recipeLength);
-
-    @Shadow
-    protected abstract void addFluidOutputLine(@NotNull FluidStack stack, long count, int recipeLength);
-
-    @Shadow
-    protected abstract void addChancedFluidOutputLine(ChancedFluidOutput output, int count, int chance, int recipeLength);
 
 }
