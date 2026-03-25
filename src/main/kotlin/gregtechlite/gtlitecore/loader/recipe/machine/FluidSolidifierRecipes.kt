1055b30c37a4c92951ffa4e3defad5608e1319e3
Magic_Sweepy <Magic_Sweepy@proton.me>
2026-03-25 09:31:06 +0800

    a little recipe fix

--- src/main/kotlin/gregtechlite/gtlitecore/loader/recipe/machine/FluidSolidifierRecipes.kt
+++ src/main/kotlin/gregtechlite/gtlitecore/loader/recipe/machine/FluidSolidifierRecipes.kt
@@ -1,13 +1,21 @@
 package gregtechlite.gtlitecore.loader.recipe.machine
 
 import gregtech.api.GTValues.L
+import gregtech.api.GTValues.LV
+import gregtech.api.GTValues.VA
+import gregtech.api.recipes.GTRecipeHandler
 import gregtech.api.recipes.RecipeMaps.FLUID_SOLIDFICATION_RECIPES
+import gregtech.api.unification.material.Materials.Blaze
 import gregtech.api.unification.material.Materials.EnderPearl
 import gregtech.api.unification.material.Materials.Glue
 import gregtech.common.items.MetaItems.SHAPE_MOLD_BALL
+import gregtech.common.items.MetaItems.SHAPE_MOLD_ROD
 import gregtech.common.items.MetaItems.STICKY_RESIN
 import gregtechlite.gtlitecore.api.SECOND
 import gregtechlite.gtlitecore.api.TICK
+import gregtechlite.gtlitecore.api.extension.EUt
+import gregtechlite.gtlitecore.api.extension.getFluid
+import gregtechlite.gtlitecore.api.extension.stack
 import gregtechlite.gtlitecore.api.unification.GTLiteMaterials.Resin
 import net.minecraft.init.Items
 import net.minecraft.item.ItemStack
@@ -45,6 +53,19 @@ internal object FluidSolidifierRecipes
             .EUt(7) // ULV
             .duration(10 * TICK)
             .buildAndRegister()
+
+        // Fix incorrect solidified amount for blaze rod.
+        GTRecipeHandler.removeRecipesByInputs(FLUID_SOLIDFICATION_RECIPES,
+            arrayOf(SHAPE_MOLD_ROD.stack()), arrayOf(Blaze.getFluid(L / 2)))
+
+        FLUID_SOLIDFICATION_RECIPES.recipeBuilder()
+            .notConsumable(SHAPE_MOLD_ROD)
+            .fluidInputs(Blaze.getFluid(L * 4))
+            .outputs(ItemStack(Items.BLAZE_ROD))
+            .EUt(VA[LV])
+            .duration(7 * SECOND + 5 * TICK)
+            .buildAndRegister()
+
     }
 
     // @formatter:on
