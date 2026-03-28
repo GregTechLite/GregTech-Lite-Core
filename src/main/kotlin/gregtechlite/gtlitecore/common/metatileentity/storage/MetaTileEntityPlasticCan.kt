375de7805d2a5cd1ac04d720ae2535cf60d3b244
Magic_Sweepy <Magic_Sweepy@proton.me>
2026-03-28 16:34:25 +0800

    clean up plastic can constructing

--- src/main/kotlin/gregtechlite/gtlitecore/common/metatileentity/storage/MetaTileEntityPlasticCan.kt
+++ src/main/kotlin/gregtechlite/gtlitecore/common/metatileentity/storage/MetaTileEntityPlasticCan.kt
@@ -6,7 +6,7 @@ import codechicken.lib.render.CCRenderState
 import codechicken.lib.render.pipeline.ColourMultiplier
 import codechicken.lib.render.pipeline.IVertexOperation
 import codechicken.lib.vec.Matrix4
-import gregtech.api.capability.GregtechDataCodes
+import gregtech.api.capability.GregtechDataCodes.UPDATE_AUTO_OUTPUT
 import gregtech.api.capability.IPropertyFluidFilter
 import gregtech.api.capability.impl.FilteredFluidHandler
 import gregtech.api.capability.impl.GTFluidHandlerItemStack
@@ -39,49 +39,29 @@ import net.minecraftforge.fluids.capability.CapabilityFluidHandler
 import net.minecraftforge.fml.relauncher.Side
 import net.minecraftforge.fml.relauncher.SideOnly
 import org.apache.commons.lang3.tuple.Pair
-import java.io.IOException
 
-class MetaTileEntityPlasticCan : MetaTileEntity
+class MetaTileEntityPlasticCan(id: ResourceLocation, private val fluidFilter: IPropertyFluidFilter<*>?,
+                               private val color: Int, private val tankSize: Int) : MetaTileEntity(id)
 {
 
-    private val fluidFilter: IPropertyFluidFilter<*>?
-    private val color: Int
-    private val tankSize: Int
     private var fluidTank: FilteredFluidHandler? = null
     private var isAutoOutput = false
 
-    constructor(id: ResourceLocation,
-                material: Material,
-                tankSize: Int) : super(id)
+    init
     {
-        val filter: IPropertyFluidFilter<*> = material.getProperty(PropertyKey.FLUID_PIPE)
-        requireNotNull(filter) {
-            "Material '$material' requires FluidPipeProperty for Plastic Cans"
+        // Protective validation for `fluidFilter`, if addon mod sent null `fluidFilter`, then throws exception.
+        requireNotNull(fluidFilter) {
+            "Material requires FluidPipeProperty for Plastic Cans"
         }
-        this.fluidFilter = filter
 
-        this.color = material.materialRGB
-        this.tankSize = tankSize
-        this.initializeInventory()
+        initializeInventory()
     }
 
-    constructor(id: ResourceLocation,
-                fluidFilter: IPropertyFluidFilter<*>,
-                color: Int,
-                tankSize: Int) : super(id)
-    {
-        this.fluidFilter = fluidFilter
-        this.color = color
-        this.tankSize = tankSize
-        this.initializeInventory()
-    }
+    constructor(id: ResourceLocation, material: Material, tankSize: Int)
+            : this(id, material.getProperty(PropertyKey.FLUID_PIPE), material.materialRGB, tankSize)
 
     override fun createMetaTileEntity(te: IGregTechTileEntity): MetaTileEntity
-        = MetaTileEntityPlasticCan(metaTileEntityId, fluidFilter!!, color, tankSize)
-
-    override fun getHarvestTool(): String = "wrench"
-
-    override fun hasFrontFacing(): Boolean = false
+        = MetaTileEntityPlasticCan(metaTileEntityId, fluidFilter, color, tankSize)
 
     override fun initializeInventory()
     {
@@ -103,6 +83,11 @@ class MetaTileEntityPlasticCan : MetaTileEntity
         }
     }
 
+    override fun initItemStackCapabilities(itemStack: ItemStack): ICapabilityProvider
+    {
+        return (GTFluidHandlerItemStack(itemStack, tankSize)).setFilter(fluidTank!!.filter)
+    }
+
     override fun writeItemStackData(itemStack: NBTTagCompound)
     {
         super.writeItemStackData(itemStack)
@@ -113,11 +98,6 @@ class MetaTileEntityPlasticCan : MetaTileEntity
             fluidStack.writeToNBT(tagCompound)
             itemStack.setTag("Fluid", tagCompound)
         }
-    }
-
-    override fun initItemStackCapabilities(itemStack: ItemStack): ICapabilityProvider
-    {
-        return (GTFluidHandlerItemStack(itemStack, tankSize)).setFilter(fluidTank!!.filter)
     }
 
     override fun writeInitialSyncData(buf: PacketBuffer)
@@ -141,15 +121,8 @@ class MetaTileEntityPlasticCan : MetaTileEntity
         var fluidStack: FluidStack? = null
         if (buf.readBoolean())
         {
-            try
-            {
-                val tagCompound = buf.readCompoundTag()
-                fluidStack = FluidStack.loadFluidStackFromNBT(tagCompound)
-            }
-            catch (_: IOException)
-            {
-                // ...
-            }
+            val tagCompound = buf.readCompoundTag()
+            fluidStack = FluidStack.loadFluidStackFromNBT(tagCompound)
         }
 
         fluidTank!!.setFluid(fluidStack)
@@ -159,130 +132,12 @@ class MetaTileEntityPlasticCan : MetaTileEntity
     override fun receiveCustomData(dataId: Int, buf: PacketBuffer)
     {
         super.receiveCustomData(dataId, buf)
-        if (dataId == GregtechDataCodes.UPDATE_AUTO_OUTPUT)
+        if (dataId == UPDATE_AUTO_OUTPUT)
         {
             isAutoOutput = buf.readBoolean()
             scheduleRenderUpdate()
         }
     }
-
-    override fun update()
-    {
-        super.update()
-        if (!world.isRemote && isAutoOutput && offsetTimer % (5 * TICK) == 0L)
-        {
-            pushFluidsIntoNearbyHandlers(*arrayOf(EnumFacing.DOWN))
-        }
-    }
-
-    override fun onRightClick(playerIn: EntityPlayer,
-                              hand: EnumHand,
-                              facing: EnumFacing?,
-                              hitResult: CuboidRayTraceResult?): Boolean
-    {
-        return if (!playerIn.getHeldItem(hand)
-            .hasCapability(CapabilityFluidHandler.FLUID_HANDLER_ITEM_CAPABILITY, null))
-        {
-            super.onRightClick(playerIn, hand, facing, hitResult)
-        }
-        else
-        {
-            world.isRemote || !playerIn.isSneaking
-                    && FluidUtil.interactWithFluidHandler(playerIn, hand, fluidTank!!)
-        }
-    }
-
-    override fun onScrewdriverClick(playerIn: EntityPlayer,
-                                    hand: EnumHand?,
-                                    wrenchSide: EnumFacing?,
-                                    hitResult: CuboidRayTraceResult?): Boolean
-    {
-        if (!playerIn.isSneaking)
-        {
-            if (world.isRemote)
-            {
-                this.scheduleRenderUpdate()
-                return true
-            }
-            else
-            {
-                playerIn.sendStatusMessage(TextComponentTranslation(
-                    "gregtech.machine.drum." + (if (isAutoOutput) "disable" else "enable") + "_output"), true)
-                toggleOutput()
-                return true
-            }
-        }
-        else
-        {
-            return super.onScrewdriverClick(playerIn, hand, wrenchSide, hitResult)
-        }
-    }
-
-    private fun toggleOutput()
-    {
-        isAutoOutput = !isAutoOutput
-        if (!world.isRemote)
-        {
-            notifyBlockUpdate()
-            writeCustomData(GregtechDataCodes.UPDATE_AUTO_OUTPUT) { it.writeBoolean(isAutoOutput) }
-            markDirty()
-        }
-    }
-
-    @SideOnly(Side.CLIENT)
-    override fun getParticleTexture(): Pair<TextureAtlasSprite?, Int?>
-    {
-        val color = GTUtility.convertOpaqueRGBA_CLtoRGB(
-            ColourRGBA.multiply(GTUtility.convertRGBtoOpaqueRGBA_CL(color),
-                                GTUtility.convertRGBtoOpaqueRGBA_CL(paintingColorForRendering)))
-        return Pair.of(GTLiteOverlays.PLASTIC_CAN.particleTexture, color)
-    }
-
-    @SideOnly(Side.CLIENT)
-    override fun renderMetaTileEntity(renderState: CCRenderState?,
-                                      translation: Matrix4?,
-                                      pipeline: Array<IVertexOperation?>?)
-    {
-        val multiplier = ColourMultiplier(
-            ColourRGBA.multiply(GTUtility.convertRGBtoOpaqueRGBA_CL(color),
-                                GTUtility.convertRGBtoOpaqueRGBA_CL(paintingColorForRendering)))
-        GTLiteOverlays.PLASTIC_CAN.render(renderState, translation, pipeline?.add(multiplier), frontFacing)
-        GTLiteOverlays.PLASTIC_CAN_OVERLAY.render(renderState, translation, pipeline)
-        if (this.isAutoOutput)
-            Textures.STEAM_VENT_OVERLAY.renderSided(EnumFacing.DOWN, renderState, translation, pipeline)
-    }
-
-    override fun getDefaultPaintingColor(): Int = 16777215
-
-    @SideOnly(Side.CLIENT)
-    override fun addInformation(stack: ItemStack,
-                                player: World?,
-                                tooltip: MutableList<String?>,
-                                advanced: Boolean)
-    {
-        tooltip.add(I18n.format("gregtech.universal.tooltip.fluid_storage_capacity", this.tankSize))
-
-        this.fluidFilter!!.appendTooltips(tooltip, true, true)
-
-        if (TooltipHelper.isShiftDown())
-        {
-            tooltip.add(I18n.format("gregtech.tool_action.screwdriver.access_covers"))
-            tooltip.add(I18n.format("gregtech.tool_action.screwdriver.auto_output_down"))
-            tooltip.add(I18n.format("gregtech.tool_action.crowbar"))
-        }
-        val tagCompound = stack.tagCompound
-        if (tagCompound != null && tagCompound.hasKey("Fluid", 10))
-        {
-            val fluidStack = FluidStack.loadFluidStackFromNBT(tagCompound.getCompoundTag("Fluid"))
-            if (fluidStack == null)
-                return
-
-            tooltip.add(I18n.format("gregtech.machine.fluid_tank.fluid",
-                                    fluidStack.amount, fluidStack.fluid.getLocalizedName(fluidStack)))
-        }
-    }
-
-    override fun showToolUsages(): Boolean = false
 
     override fun writeToNBT(data: NBTTagCompound): NBTTagCompound
     {
@@ -299,6 +154,121 @@ class MetaTileEntityPlasticCan : MetaTileEntity
         isAutoOutput = data.getBoolean("AutoOutput")
     }
 
+    override fun update()
+    {
+        super.update()
+        if (!world.isRemote && isAutoOutput && offsetTimer % (5 * TICK) == 0L)
+        {
+            pushFluidsIntoNearbyHandlers(*arrayOf(EnumFacing.DOWN))
+        }
+    }
+
+    override fun getHarvestTool(): String = "wrench"
+
+    override fun hasFrontFacing(): Boolean = false
+
+    override fun onRightClick(playerIn: EntityPlayer, hand: EnumHand, facing: EnumFacing?,
+                              hitResult: CuboidRayTraceResult?): Boolean
+    {
+        return if (!playerIn.getHeldItem(hand).hasCapability(
+                CapabilityFluidHandler.FLUID_HANDLER_ITEM_CAPABILITY, null))
+        {
+            super.onRightClick(playerIn, hand, facing, hitResult)
+        }
+        else
+        {
+            world.isRemote || !playerIn.isSneaking
+                    && FluidUtil.interactWithFluidHandler(playerIn, hand, fluidTank!!)
+        }
+    }
+
+    override fun onScrewdriverClick(playerIn: EntityPlayer, hand: EnumHand?, wrenchSide: EnumFacing?,
+                                    hitResult: CuboidRayTraceResult?): Boolean
+    {
+        if (!playerIn.isSneaking)
+        {
+            if (world.isRemote)
+            {
+                scheduleRenderUpdate()
+                return true
+            }
+            else
+            {
+                playerIn.sendStatusMessage(TextComponentTranslation(
+                    "gregtech.machine.drum." + (if (isAutoOutput) "disable" else "enable") + "_output"), true)
+                toggleOutput()
+                return true
+            }
+        }
+        else
+        {
+            return super.onScrewdriverClick(playerIn, hand, wrenchSide, hitResult)
+        }
+    }
+
+    @SideOnly(Side.CLIENT)
+    override fun getParticleTexture(): Pair<TextureAtlasSprite, Int>
+    {
+        val color = GTUtility.convertOpaqueRGBA_CLtoRGB(
+            ColourRGBA.multiply(GTUtility.convertRGBtoOpaqueRGBA_CL(color),
+                                GTUtility.convertRGBtoOpaqueRGBA_CL(paintingColorForRendering)))
+        return Pair.of(GTLiteOverlays.PLASTIC_CAN.particleTexture, color)
+    }
+
+    @SideOnly(Side.CLIENT)
+    override fun renderMetaTileEntity(renderState: CCRenderState?, translation: Matrix4?,
+                                      pipeline: Array<IVertexOperation?>?)
+    {
+        val multiplier = ColourMultiplier(
+            ColourRGBA.multiply(GTUtility.convertRGBtoOpaqueRGBA_CL(color),
+                                GTUtility.convertRGBtoOpaqueRGBA_CL(paintingColorForRendering)))
+        GTLiteOverlays.PLASTIC_CAN.render(renderState, translation, pipeline?.add(multiplier), frontFacing)
+        GTLiteOverlays.PLASTIC_CAN_OVERLAY.render(renderState, translation, pipeline)
+        if (isAutoOutput)
+            Textures.STEAM_VENT_OVERLAY.renderSided(EnumFacing.DOWN, renderState, translation, pipeline)
+    }
+
+    override fun getDefaultPaintingColor(): Int = 16777215
+
+    @SideOnly(Side.CLIENT)
+    override fun addInformation(stack: ItemStack, player: World?, tooltip: MutableList<String>, advanced: Boolean)
+    {
+        tooltip.add(I18n.format("gregtech.universal.tooltip.fluid_storage_capacity", tankSize))
+
+        fluidFilter!!.appendTooltips(tooltip, true, true)
+
+        if (TooltipHelper.isShiftDown())
+        {
+            tooltip.add(I18n.format("gregtech.tool_action.screwdriver.access_covers"))
+            tooltip.add(I18n.format("gregtech.tool_action.screwdriver.auto_output_down"))
+            tooltip.add(I18n.format("gregtech.tool_action.crowbar"))
+        }
+
+        val tagCompound = stack.tagCompound
+        if (tagCompound != null && tagCompound.hasKey("Fluid", 10))
+        {
+            val fluidStack = FluidStack.loadFluidStackFromNBT(tagCompound.getCompoundTag("Fluid"))
+            if (fluidStack == null)
+                return
+
+            tooltip.add(I18n.format("gregtech.machine.fluid_tank.fluid",
+                                    fluidStack.amount, fluidStack.fluid.getLocalizedName(fluidStack)))
+        }
+    }
+
+    override fun showToolUsages(): Boolean = false
+
     override fun shouldSerializeInventories(): Boolean = false
+
+    private fun toggleOutput()
+    {
+        isAutoOutput = !isAutoOutput
+        if (!world.isRemote)
+        {
+            notifyBlockUpdate()
+            writeCustomData(UPDATE_AUTO_OUTPUT) { it.writeBoolean(isAutoOutput) }
+            markDirty()
+        }
+    }
 
 }
