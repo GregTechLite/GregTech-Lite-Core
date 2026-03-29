8fee401c815abe7958a89d0e88c445f774ad07ab
Magic_Sweepy <Magic_Sweepy@proton.me>
2026-03-29 10:26:07 +0800

    build DSL context for the recipe builder

--- src/main/kotlin/gregtechlite/gtlitecore/api/extension/RecipeBuilderExt.kt
+++ src/main/kotlin/gregtechlite/gtlitecore/api/extension/RecipeBuilderExt.kt
@@ -1,28 +1,53 @@
+@file:Suppress("FunctionName")
 package gregtechlite.gtlitecore.api.extension
 
 import gregtech.api.recipes.RecipeBuilder
+import gregtech.api.recipes.RecipeMap
 import gregtech.api.recipes.builders.ResearchRecipeBuilder
+import net.minecraft.item.Item
+import net.minecraft.item.ItemStack
 
-// Short-circuit number converts for RecipeBuilder#EUt.
+// region DSL Context
 
-@Suppress("FunctionName")
-fun <T: RecipeBuilder<T>> RecipeBuilder<T>.EUt(eut: Int): T
-{
-    return EUt(eut.toLong())
-}
+/**
+ * Registers a recipe for the [RecipeMap].
+ *
+ * This method is Kotlin style DSL for GTCEu [RecipeBuilder] for recipe registries.
+ *
+ * @param builder The corresponding [RecipeBuilder] of the [RecipeMap].
+ */
+fun <T: RecipeBuilder<T>> RecipeMap<T>.buildRecipe(builder: T.() -> Unit)
+    = recipeBuilder().apply(builder).buildAndRegister()
 
-@Suppress("FunctionName")
-fun <T: ResearchRecipeBuilder<T>> ResearchRecipeBuilder<T>.EUt(eut: Int): T
-{
-    return EUt(eut.toLong())
-}
+// endregion
+
+// region Recipe Input Shortcut
+
+fun <T: RecipeBuilder<T>> RecipeBuilder<T>.inputs(item: Item): T
+    = inputs(item, 1)
+
+fun <T: RecipeBuilder<T>> RecipeBuilder<T>.inputs(item: Item, amount: Int): T
+    = inputs(item, amount, 0)
+
+fun <T: RecipeBuilder<T>> RecipeBuilder<T>.inputs(item: Item, amount: Int, metadata: Int): T
+    = inputs(ItemStack(item, amount, metadata))
+
+// endregion
+
+// region Recipe EUt Shortcut
+
+fun <T: RecipeBuilder<T>> RecipeBuilder<T>.EUt(eut: Int): T = EUt(eut.toLong())
+
+fun <T: ResearchRecipeBuilder<T>> ResearchRecipeBuilder<T>.EUt(eut: Int): T = EUt(eut.toLong())
+
+// endregion
+
+// region Recipe Duration Shortcut
 
 fun <T: RecipeBuilder<T>> RecipeBuilder<T>.duration(duration: Long): T
-{
-    return duration(duration.toInt())
-}
+    = duration(duration.toInt())
 
 fun <T: RecipeBuilder<T>> RecipeBuilder<T>.duration(duration: Double): T
-{
-    return duration(duration.toInt())
-}
+    = duration(duration.toInt())
+
+// endregion
