0a544f5a1a113f1a18f7285bb39d7677f30a075f
Magic_Sweepy <Magic_Sweepy@proton.me>
2026-03-30 18:07:56 +0800

    merge some extension and expand it

--- src/main/kotlin/gregtechlite/gtlitecore/api/extension/ItemStackExt.kt
+++ src/main/kotlin/gregtechlite/gtlitecore/api/extension/ItemStackExt.kt
@@ -1,13 +1,17 @@
 package gregtechlite.gtlitecore.api.extension
 
+import gregtech.api.items.metaitem.MetaItem
+import gregtech.api.items.metaitem.MetaOreDictItem
+import gregtech.api.metatileentity.MetaTileEntity
 import net.minecraft.item.Item
 import net.minecraft.item.ItemStack
+
+// region Item Stack Copying
 
 /**
  * Copies the [ItemStack] with new stack size.
  *
- * @param count New stack size of copying ItemStack.
- * @return      Copied ItemStack with a [count].
+ * @param count The new stack size of the [ItemStack].
  */
 fun ItemStack.copy(count: Int = 1): ItemStack
 {
@@ -19,9 +23,8 @@ fun ItemStack.copy(count: Int = 1): ItemStack
 /**
  * Copies the [ItemStack] with new stack size.
  *
- * @param meta  Metadata of ItemStack.
- * @param count New stack size of copying ItemStack.
- * @return      Copied ItemStack with a [count] and [meta].
+ * @param meta  The metadata of the [ItemStack].
+ * @param count The new stack size of the [ItemStack].
  */
 fun ItemStack.copy(meta: Int, count: Int = 1): ItemStack
 {
@@ -30,6 +33,10 @@ fun ItemStack.copy(meta: Int, count: Int = 1): ItemStack
     stack.count = count
     return stack
 }
+
+// endregion
+
+// region Item Stack Shortcut
 
 /**
  * Gets the [ItemStack] of the standard [Item].
@@ -48,3 +55,24 @@ fun Item.stack(meta: Int = 0): ItemStack = getStack(meta = meta)
 fun Item.getStack(count: Int = 1, meta: Int = 0): ItemStack {
     return ItemStack(this, count, meta)
 }
+
+fun MetaItem<*>.MetaValueItem.stack(): ItemStack = getStack()
+
+fun MetaItem<*>.MetaValueItem.getStack(count: Int = 1): ItemStack = getStackForm(count)
+
+fun MetaItem<*>.MetaValueItem.addOreDicts(vararg oreDictNames: String): MetaItem<*>.MetaValueItem
+{
+    for (oreDictName in oreDictNames)
+        addOreDict(oreDictName)
+    return this
+}
+
+fun MetaOreDictItem.OreDictValueItem.stack(): ItemStack = getStack(1)
+
+fun MetaOreDictItem.OreDictValueItem.getStack(count: Int): ItemStack = getItemStack(count)
+
+fun MetaTileEntity.stack(): ItemStack = getStack()
+
+fun MetaTileEntity.getStack(count: Int = 1) = stackForm.copy(count)
+
+// endregion
