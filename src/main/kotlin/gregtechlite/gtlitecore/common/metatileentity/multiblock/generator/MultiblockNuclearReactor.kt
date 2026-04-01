b1fb6421ad2d734351a8ee5728ed6c8e061a8202
Magic_Sweepy <Magic_Sweepy@proton.me>
2026-04-01 12:38:39 +0800

    refactor: Allow Nuclear Reactor void excess energy and boost production (#75)
    * let boosted factor applying with boostProduction
    
    * add excess mode to void energy for dynamo hatches
    
    * excess mode tooltip details
    
    * fix null sync when catch fluid needed
    
    * unify serialized param names & default disable

--- src/main/kotlin/gregtechlite/gtlitecore/common/metatileentity/multiblock/generator/MultiblockNuclearReactor.kt
+++ src/main/kotlin/gregtechlite/gtlitecore/common/metatileentity/multiblock/generator/MultiblockNuclearReactor.kt
@@ -1,5 +1,8 @@
 package gregtechlite.gtlitecore.common.metatileentity.multiblock.generator
 
+import com.cleanroommc.modularui.api.drawable.IKey
+import com.cleanroommc.modularui.value.sync.BooleanSyncValue
+import com.cleanroommc.modularui.widgets.ToggleButton
 import gregtech.api.GTValues.EV
 import gregtech.api.GTValues.MAX
 import gregtech.api.GTValues.V
@@ -13,11 +16,14 @@ import gregtech.api.metatileentity.multiblock.MultiblockAbility.MUFFLER_HATCH
 import gregtech.api.metatileentity.multiblock.MultiblockAbility.OUTPUT_ENERGY
 import gregtech.api.metatileentity.multiblock.MultiblockAbility.SUBSTATION_OUTPUT_ENERGY
 import gregtech.api.metatileentity.multiblock.RecipeMapMultiblockController
+import gregtech.api.metatileentity.multiblock.ui.MultiblockUIBuilder
+import gregtech.api.metatileentity.multiblock.ui.MultiblockUIFactory
 import gregtech.api.pattern.BlockPattern
 import gregtech.api.pattern.FactoryBlockPattern
 import gregtech.api.pattern.PatternMatchContext
 import gregtech.client.renderer.ICubeRenderer
 import gregtechlite.gtlitecore.api.GTLiteAPI.NUCLEAR_REACTOR_CORE_TIER
+import gregtechlite.gtlitecore.api.gui.GTLiteMuiTextures
 import gregtechlite.gtlitecore.api.pattern.TraceabilityPredicates.getAttributeOrDefault
 import gregtechlite.gtlitecore.api.pattern.TraceabilityPredicates.nuclearReactorCores
 import gregtechlite.gtlitecore.api.recipe.GTLiteRecipeMaps.NUCLEAR_FUELS
@@ -27,34 +33,19 @@ import gregtechlite.gtlitecore.common.block.variant.GlassCasing
 import gregtechlite.gtlitecore.common.block.variant.MetalCasing
 import net.minecraft.client.resources.I18n
 import net.minecraft.item.ItemStack
+import net.minecraft.nbt.NBTTagCompound
+import net.minecraft.network.PacketBuffer
 import net.minecraft.util.EnumParticleTypes
 import net.minecraft.util.ResourceLocation
 import net.minecraft.world.World
 import net.minecraftforge.fml.relauncher.Side
 import net.minecraftforge.fml.relauncher.SideOnly
-import kotlin.math.max
 
-/**
- * Reactor Core buff factors:
- * - Thorium 1.05x
- * - Protactinium 1.1x
- * - Uranium 1.3x
- * - Neptunium 1.4x
- * - Plutonium 1.5x
- * - Americium 1.8x
- * - Curium 1.85x
- * - Berkelium 1.9x
- * - Californium 2.0x
- * - Einsteinium 2.5x
- * - Fermium 3x
- * - Mendelevium 3.2x
- *
- * TODO: FIXME: Seems reactor core buff not effect.
- */
 class MultiblockNuclearReactor(id: ResourceLocation) : FuelMultiblockController(id, NUCLEAR_FUELS, EV)
 {
 
     private var coreTier = 0
+    private var isExcessMode = false
 
     init
     {
@@ -131,6 +122,7 @@ class MultiblockNuclearReactor(id: ResourceLocation) : FuelMultiblockController(
         tooltip.add(I18n.format("gtlitecore.machine.nuclear_reactor.tooltip.5"))
         tooltip.add(I18n.format("gtlitecore.machine.nuclear_reactor.tooltip.6"))
         tooltip.add(I18n.format("gtlitecore.machine.nuclear_reactor.tooltip.7"))
+        tooltip.add(I18n.format("gtlitecore.tooltip.machine.excess_mode"))
     }
 
     override fun canBeDistinct() = false
@@ -139,34 +131,105 @@ class MultiblockNuclearReactor(id: ResourceLocation) : FuelMultiblockController(
 
     override fun getMufflerParticle(): EnumParticleTypes = EnumParticleTypes.CLOUD
 
+    override fun createUIFactory(): MultiblockUIFactory
+    {
+        return super.createUIFactory()
+            .createFlexButton { guiData, guiSyncManager ->
+                val excessMode = BooleanSyncValue(::getExcessMode, ::setExcessMode)
+                return@createFlexButton ToggleButton()
+                    .size(18)
+                    .disableHoverBackground()
+                    .overlay(false, GTLiteMuiTextures.BUTTON_EXCESS_MODE[0])
+                    .overlay(true, GTLiteMuiTextures.BUTTON_EXCESS_MODE[1])
+                    .addTooltip(false, IKey.lang("gtlitecore.tooltip.gui.excess_mode.disabled"))
+                    .addTooltip(true, IKey.lang("gtlitecore.tooltip.gui.excess_mode.enabled"))
+                    .value(excessMode)
+            }
+    }
+
+    override fun configureDisplayText(builder: MultiblockUIBuilder)
+    {
+        builder.setWorkingStatus(recipeMapWorkable.isWorkingEnabled, recipeMapWorkable.isActive)
+        builder.addEnergyProductionLine(recipeMapWorkable.maxVoltage, recipeMapWorkable.recipeEUt)
+        builder.addWorkingStatusLine()
+    }
+
+    override fun writeToNBT(data: NBTTagCompound): NBTTagCompound
+    {
+        super.writeToNBT(data)
+        data.setBoolean("excessMode", isExcessMode)
+        return data
+    }
+
+    override fun readFromNBT(data: NBTTagCompound)
+    {
+        super.readFromNBT(data)
+        isExcessMode = data.getBoolean("excessMode")
+    }
+
+    override fun writeInitialSyncData(buf: PacketBuffer)
+    {
+        super.writeInitialSyncData(buf)
+        buf.writeBoolean(isExcessMode)
+    }
+
+    override fun receiveInitialSyncData(buf: PacketBuffer)
+    {
+        super.receiveInitialSyncData(buf)
+        isExcessMode = buf.readBoolean()
+    }
+
     /**
      * Get boosted factor from [coreTier] to give reactor production a boost.
+     *
+     * | Reactor Core | Boosted Factor |
+     * |--------------|----------------|
+     * | Thorium      | 1.05x          |
+     * | Protactinium | 1.1x           |
+     * | Uranium      | 1.3x           |
+     * | Neptunium    | 1.4x           |
+     * | Plutonium    | 1.5x           |
+     * | Americium    | 1.8x           |
+     * | Curium       | 1.85x          |
+     * | Berkelium    | 1.9x           |
+     * | Californium  | 2.0x           |
+     * | Einsteinium  | 2.5x           |
+     * | Fermium      | 3x             |
+     * | Mendelevium  | 3.2x           |
      */
-    fun getBoostedFromCoreTier(tier: Int): Double = when (tier)
+    private fun getBoostedFromCoreTier(tier: Int): Double = when (tier)
     {
         (1) -> 1.05 // Th
-        (2) -> 1.1 // Pa
-        (3) -> 1.3 // U
-        (4) -> 1.4 // Np
-        (5) -> 1.5 // Pu
-        (6) -> 1.8 // Am
+        (2) -> 1.1  // Pa
+        (3) -> 1.3  // U
+        (4) -> 1.4  // Np
+        (5) -> 1.5  // Pu
+        (6) -> 1.8  // Am
         (7) -> 1.85 // Cm
-        (8) -> 1.9 // Bk
-        (9) -> 2.0 // Cf
+        (8) -> 1.9  // Bk
+        (9) -> 2.0  // Cf
         (10) -> 2.5 // Es
         (11) -> 3.0 // Fm
         (12) -> 3.2 // Md
-        else -> 0.0 // Ensure it not cause problem for workableHandler progress max value.
+        else -> 1.0 // No boost without a valid reactor core
+    }
+
+    private fun getExcessMode() = isExcessMode
+
+    private fun setExcessMode(excessMode: Boolean)
+    {
+        isExcessMode = excessMode
     }
 
     private inner class NuclearReactorWorkableHandler(mte: RecipeMapMultiblockController)
         : MultiblockFuelRecipeLogic(mte)
     {
 
-        override fun getMaxVoltage(): Long = max(super.getMaxVoltage().toDouble(),
-                                                 super.getMaxVoltage() * getBoostedFromCoreTier(coreTier)).toLong()
+        override fun boostProduction(production: Long): Long
+            = (production * getBoostedFromCoreTier(coreTier)).toLong()
 
-        override fun getParallelLimit() = Int.MAX_VALUE
+        override fun drawEnergy(recipeEUt: Long, simulate: Boolean): Boolean
+            = if (isExcessMode) true else super.drawEnergy(recipeEUt, simulate)
 
     }
 
