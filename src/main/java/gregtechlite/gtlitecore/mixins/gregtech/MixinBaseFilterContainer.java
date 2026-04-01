2f1dce8b6e2d8f4e066f06d91e13e0edb5a88781
Magic_Sweepy <Magic_Sweepy@proton.me>
2026-04-01 15:55:38 +0800

    add missing compat annotation for the mixin

--- src/main/java/gregtechlite/gtlitecore/mixins/gregtech/MixinBaseFilterContainer.java
+++ src/main/java/gregtechlite/gtlitecore/mixins/gregtech/MixinBaseFilterContainer.java
@@ -3,11 +3,13 @@ package gregtechlite.gtlitecore.mixins.gregtech;
 import com.cleanroommc.modularui.api.ISyncedAction;
 import com.cleanroommc.modularui.value.sync.PanelSyncManager;
 import gregtech.common.covers.filter.BaseFilterContainer;
+import gregtechlite.gtlitecore.mixins.Compat;
 import net.minecraftforge.fml.relauncher.Side;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
+@Compat(modId = { "gregtech", "modularui" })
 @Mixin(value = BaseFilterContainer.class, remap = false)
 public abstract class MixinBaseFilterContainer {
 
