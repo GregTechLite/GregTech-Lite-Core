b4d7bc8309ba38846a6c9e42315aa84f686245da
Magic_Sweepy <Magic_Sweepy@proton.me>
2026-04-01 10:12:20 +0800

    a little clean

--- src/main/java/gregtechlite/gtlitecore/mixins/gregtech/MixinMetaTileEntityCleaningMaintenanceHatch.java
+++ src/main/java/gregtechlite/gtlitecore/mixins/gregtech/MixinMetaTileEntityCleaningMaintenanceHatch.java
@@ -5,6 +5,7 @@ import gregtech.common.metatileentities.multi.multiblockpart.MetaTileEntityAutoM
 import gregtech.common.metatileentities.multi.multiblockpart.MetaTileEntityCleaningMaintenanceHatch;
 import net.minecraft.util.ResourceLocation;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 
 @Mixin(value = MetaTileEntityCleaningMaintenanceHatch.class, remap = false)
 public abstract class MixinMetaTileEntityCleaningMaintenanceHatch extends MetaTileEntityAutoMaintenanceHatch
@@ -15,6 +16,14 @@ public abstract class MixinMetaTileEntityCleaningMaintenanceHatch extends MetaTi
         super(metaTileEntityId);
     }
 
+    /**
+     * Because we enabled the cleaning multiblock config, we think the Cleaning Maintenance Hatch is too expensive
+     * than its efficiency, so we want to down-tier it to a cheaper voltage requirement.
+     *
+     * @author Magic_Sweepy
+     * @reason Down-tier the Cleaning Maintenance Hatch from UV to IV.
+     */
+    @Overwrite
     @Override
     public int getTier()
     {
