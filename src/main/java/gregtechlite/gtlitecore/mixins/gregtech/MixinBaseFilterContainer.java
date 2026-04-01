cb8a01a2b8c9cd4f99bb962898ccc05364259c2a
Magic_Sweepy <Magic_Sweepy@proton.me>
2026-04-01 15:53:34 +0800

    fix filter container incorrect synced action register

---
+++ src/main/java/gregtechlite/gtlitecore/mixins/gregtech/MixinBaseFilterContainer.java
@@ -0,0 +1,20 @@
+package gregtechlite.gtlitecore.mixins.gregtech;
+
+import com.cleanroommc.modularui.api.ISyncedAction;
+import com.cleanroommc.modularui.value.sync.PanelSyncManager;
+import gregtech.common.covers.filter.BaseFilterContainer;
+import net.minecraftforge.fml.relauncher.Side;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(value = BaseFilterContainer.class, remap = false)
+public abstract class MixinBaseFilterContainer {
+
+    @Redirect(method = "initUI",
+              at = @At(value = "INVOKE",
+                       target = "Lcom/cleanroommc/modularui/value/sync/PanelSyncManager;registerSyncedAction(Ljava/lang/String;Lcom/cleanroommc/modularui/api/ISyncedAction;)Lcom/cleanroommc/modularui/value/sync/PanelSyncManager;"))
+    private PanelSyncManager redirectSyncedAction(PanelSyncManager manager, String mapKey, ISyncedAction syncedAction) {
+        return manager.registerSyncedAction(mapKey, Side.CLIENT, syncedAction);
+    }
+}
