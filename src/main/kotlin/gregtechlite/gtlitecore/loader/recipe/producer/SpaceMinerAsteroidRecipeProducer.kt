7367d80ddc2de5f4b4a48b97d20def2d6ee93446
Magic_Sweepy <Magic_Sweepy@proton.me>
2026-05-21 11:37:19 +0800

    pick some useful asteroid from drone asteroids

--- src/main/kotlin/gregtechlite/gtlitecore/loader/recipe/producer/SpaceMinerAsteroidRecipeProducer.kt
+++ src/main/kotlin/gregtechlite/gtlitecore/loader/recipe/producer/SpaceMinerAsteroidRecipeProducer.kt
@@ -2,39 +2,96 @@ package gregtechlite.gtlitecore.loader.recipe.producer
 
 import gregtech.api.GTValues.IV
 import gregtech.api.GTValues.VA
+import gregtech.api.unification.material.Materials.Almandine
+import gregtech.api.unification.material.Materials.Amethyst
+import gregtech.api.unification.material.Materials.Andradite
 import gregtech.api.unification.material.Materials.Antimony
+import gregtech.api.unification.material.Materials.Apatite
+import gregtech.api.unification.material.Materials.Asbestos
+import gregtech.api.unification.material.Materials.Bauxite
+import gregtech.api.unification.material.Materials.Bentonite
+import gregtech.api.unification.material.Materials.Biotite
+import gregtech.api.unification.material.Materials.BlueTopaz
 import gregtech.api.unification.material.Materials.Cadmium
+import gregtech.api.unification.material.Materials.CertusQuartz
 import gregtech.api.unification.material.Materials.Charcoal
+import gregtech.api.unification.material.Materials.Cinnabar
 import gregtech.api.unification.material.Materials.Coal
 import gregtech.api.unification.material.Materials.Cobaltite
 import gregtech.api.unification.material.Materials.Coke
 import gregtech.api.unification.material.Materials.Copper
 import gregtech.api.unification.material.Materials.Diamond
+import gregtech.api.unification.material.Materials.Diatomite
+import gregtech.api.unification.material.Materials.Emerald
+import gregtech.api.unification.material.Materials.GarnetRed
+import gregtech.api.unification.material.Materials.GarnetYellow
 import gregtech.api.unification.material.Materials.Gold
 import gregtech.api.unification.material.Materials.Graphite
+import gregtech.api.unification.material.Materials.GreenSapphire
+import gregtech.api.unification.material.Materials.Grossular
 import gregtech.api.unification.material.Materials.Iron
+import gregtech.api.unification.material.Materials.Kyanite
+import gregtech.api.unification.material.Materials.Lapis
+import gregtech.api.unification.material.Materials.Lazurite
 import gregtech.api.unification.material.Materials.Lead
+import gregtech.api.unification.material.Materials.Lepidolite
+import gregtech.api.unification.material.Materials.Mica
 import gregtech.api.unification.material.Materials.Nickel
+import gregtech.api.unification.material.Materials.Oilsands
+import gregtech.api.unification.material.Materials.Olivine
+import gregtech.api.unification.material.Materials.Opal
+import gregtech.api.unification.material.Materials.Pollucite
+import gregtech.api.unification.material.Materials.Pyrope
+import gregtech.api.unification.material.Materials.Quartzite
+import gregtech.api.unification.material.Materials.Realgar
+import gregtech.api.unification.material.Materials.RockSalt
+import gregtech.api.unification.material.Materials.Ruby
+import gregtech.api.unification.material.Materials.Salt
+import gregtech.api.unification.material.Materials.Saltpeter
+import gregtech.api.unification.material.Materials.Sapphire
 import gregtech.api.unification.material.Materials.Silver
+import gregtech.api.unification.material.Materials.Sodalite
+import gregtech.api.unification.material.Materials.Spessartine
+import gregtech.api.unification.material.Materials.Sphalerite
+import gregtech.api.unification.material.Materials.Spodumene
+import gregtech.api.unification.material.Materials.Sulfur
 import gregtech.api.unification.material.Materials.Tin
+import gregtech.api.unification.material.Materials.Topaz
+import gregtech.api.unification.material.Materials.Trona
+import gregtech.api.unification.material.Materials.Uvarovite
+import gregtech.api.unification.material.Materials.Zircon
 import gregtech.api.unification.ore.OrePrefix.block
 import gregtech.api.unification.ore.OrePrefix.oreEndstone
 import gregtechlite.gtlitecore.api.SECOND
 import gregtechlite.gtlitecore.api.extension.EUt
 import gregtechlite.gtlitecore.api.extension.addRecipe
 import gregtechlite.gtlitecore.api.recipe.GTLiteRecipeMaps.SPACE_MINER_RECIPES
+import gregtechlite.gtlitecore.api.unification.GTLiteMaterials.Albite
+import gregtechlite.gtlitecore.api.unification.GTLiteMaterials.Augite
+import gregtechlite.gtlitecore.api.unification.GTLiteMaterials.Bytownite
 import gregtechlite.gtlitecore.api.unification.GTLiteMaterials.Canfieldite
 import gregtechlite.gtlitecore.api.unification.GTLiteMaterials.Digenite
+import gregtechlite.gtlitecore.api.unification.GTLiteMaterials.Dolomite
+import gregtechlite.gtlitecore.api.unification.GTLiteMaterials.Ferrosilite
 import gregtechlite.gtlitecore.api.unification.GTLiteMaterials.Firestone
+import gregtechlite.gtlitecore.api.unification.GTLiteMaterials.Kaolinite
+import gregtechlite.gtlitecore.api.unification.GTLiteMaterials.Labradorite
 import gregtechlite.gtlitecore.api.unification.GTLiteMaterials.Lignite
+import gregtechlite.gtlitecore.api.unification.GTLiteMaterials.Muscovite
+import gregtechlite.gtlitecore.api.unification.GTLiteMaterials.Oligoclase
+import gregtechlite.gtlitecore.api.unification.GTLiteMaterials.Orpiment
 import gregtechlite.gtlitecore.api.unification.GTLiteMaterials.Otavite
 import gregtechlite.gtlitecore.api.unification.GTLiteMaterials.Paraffin
+import gregtechlite.gtlitecore.api.unification.GTLiteMaterials.Phlogopite
+import gregtechlite.gtlitecore.api.unification.GTLiteMaterials.Prasiolite
 import gregtechlite.gtlitecore.api.unification.GTLiteMaterials.RichHematite
 import gregtechlite.gtlitecore.api.unification.GTLiteMaterials.Skutterudite
 import gregtechlite.gtlitecore.api.unification.GTLiteMaterials.Stibiconite
 import gregtechlite.gtlitecore.api.unification.GTLiteMaterials.Sylvanite
+import gregtechlite.gtlitecore.api.unification.GTLiteMaterials.Tanzanite
 import gregtechlite.gtlitecore.api.unification.GTLiteMaterials.Trevorite
 import gregtechlite.gtlitecore.api.unification.GTLiteMaterials.Vanadinite
+import gregtechlite.gtlitecore.api.unification.GTLiteMaterials.Wollastonite
 import gregtechlite.gtlitecore.common.item.GTLiteMetaItems.MINING_DRONE_IV
 
 object SpaceMinerAsteroidRecipeProducer
@@ -147,25 +204,139 @@ object SpaceMinerAsteroidRecipeProducer
             tier(1)
         }
 
+        // Salt-Sulfur Asteroid
+        SPACE_MINER_RECIPES.addRecipe {
+            circuitMeta(6)
+            notConsumable(MINING_DRONE_IV)
+            output(oreEndstone, Salt, 1024)
+            output(oreEndstone, RockSalt, 1024)
+            output(oreEndstone, Saltpeter, 1024)
+            output(oreEndstone, Sulfur, 1024)
+            output(oreEndstone, Sphalerite, 1024)
+            output(oreEndstone, Lepidolite, 1024)
+            output(oreEndstone, Spodumene, 1024)
+            output(oreEndstone, Asbestos, 1024)
+            output(oreEndstone, Diatomite, 1024)
+            output(oreEndstone, Oilsands, 1024)
+            EUt(VA[IV])
+            duration(20 * SECOND)
+            CWUt(4)
+            tier(1)
+        }
+
+        // Misc Gems I Asteroid
+        SPACE_MINER_RECIPES.addRecipe {
+            circuitMeta(7)
+            notConsumable(MINING_DRONE_IV)
+            output(oreEndstone, Albite, 1024)
+            output(oreEndstone, Oligoclase, 1024)
+            output(oreEndstone, Prasiolite, 1024)
+            output(oreEndstone, Labradorite, 1024)
+            output(oreEndstone, Bytownite, 1024)
+            output(oreEndstone, Cinnabar, 1024)
+            output(oreEndstone, Ruby, 1024)
+            output(oreEndstone, Emerald, 1024)
+            output(oreEndstone, Olivine, 1024)
+            output(oreEndstone, Sapphire, 1024)
+            output(oreEndstone, GreenSapphire, 1024)
+            output(oreEndstone, Lapis, 1024)
+            output(oreEndstone, Lazurite, 1024)
+            output(oreEndstone, Sodalite, 1024)
+            output(oreEndstone, CertusQuartz, 1024)
+            output(oreEndstone, Topaz, 1024)
+            EUt(VA[IV])
+            duration(20 * SECOND)
+            CWUt(5)
+            tier(1)
+        }
+
+        // Misc Gems II Asteroid
+        SPACE_MINER_RECIPES.addRecipe {
+            circuitMeta(8)
+            notConsumable(MINING_DRONE_IV)
+            output(oreEndstone, BlueTopaz, 1024)
+            output(oreEndstone, Amethyst, 1024)
+            output(oreEndstone, Opal, 1024)
+            output(oreEndstone, Almandine, 1024)
+            output(oreEndstone, Andradite, 1024)
+            output(oreEndstone, Grossular, 1024)
+            output(oreEndstone, Pyrope, 1024)
+            output(oreEndstone, Spessartine, 1024)
+            output(oreEndstone, Uvarovite, 1024)
+            output(oreEndstone, Tanzanite, 1024)
+            output(oreEndstone, Quartzite, 1024)
+            output(oreEndstone, Realgar, 1024)
+            output(oreEndstone, Orpiment, 1024)
+            output(oreEndstone, Apatite, 1024)
+            output(oreEndstone, GarnetRed, 1024)
+            output(oreEndstone, GarnetYellow, 1024)
+            EUt(VA[IV])
+            duration(20 * SECOND)
+            CWUt(5)
+            tier(1)
+        }
+
+        // Mica-Kaolinite-Dolomite Asteroid
+        SPACE_MINER_RECIPES.addRecipe {
+            circuitMeta(9)
+            notConsumable(MINING_DRONE_IV)
+            output(oreEndstone, Mica, 1024)
+            output(oreEndstone, Kyanite, 1024)
+            output(oreEndstone, Bauxite, 1024)
+            output(oreEndstone, Pollucite, 1024)
+            output(oreEndstone, Muscovite, 1024)
+            output(oreEndstone, Phlogopite, 1024)
+            output(oreEndstone, Biotite, 1024)
+            output(oreEndstone, Lepidolite, 1024)
+            output(oreEndstone, Kaolinite, 1024)
+            output(oreEndstone, Bentonite, 1024)
+            output(oreEndstone, Augite, 1024)
+            output(oreEndstone, Ferrosilite, 1024)
+            output(oreEndstone, Spodumene, 1024)
+            output(oreEndstone, Dolomite, 1024)
+            output(oreEndstone, Wollastonite, 1024)
+            output(oreEndstone, Trona, 1024)
+            EUt(VA[IV])
+            duration(20 * SECOND)
+            CWUt(6)
+            tier(1)
+        }
+
         // endregion
 
         // region Legendary Asteroid (LuV)
+
+        // Aluminium, Manganese, Chrome
 
         // endregion
 
         // region Exotic Asteroid (ZPM)
 
+        // TODO Zirc output(oreEndstone, Zircon, 1024)
+        // Radioactive
+        // enriched naquadah, trinium
+        // plat group
+
         // endregion
 
         // region Perfect Asteroid (UV)
+
+        // rare earth
+        // bedrockium
+
+        // naquadria
 
         // endregion
 
         // region Tipler Asteroid (UHV)
 
+        // adamantium
+
         // endregion
 
         // region Gallifreyan Asteroid (UEV)
+
+        // vibranium
 
         // endregion
 
