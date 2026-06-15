fd1d2100d67cc6ba9c0b4715402c6223f4fde81c
Magic_Sweepy <Magic_Sweepy@proton.me>
2026-06-15 20:29:51 +0800

    move gms process to additives processing class

--- src/main/kotlin/gregtechlite/gtlitecore/loader/recipe/foodprocessing/NutrientPasteMealProcessing.kt
+++ src/main/kotlin/gregtechlite/gtlitecore/loader/recipe/foodprocessing/NutrientPasteMealProcessing.kt
@@ -3,22 +3,15 @@ package gregtechlite.gtlitecore.loader.recipe.foodprocessing
 import gregtech.api.GTValues.EV
 import gregtech.api.GTValues.HV
 import gregtech.api.GTValues.IV
-import gregtech.api.GTValues.LV
 import gregtech.api.GTValues.ULV
 import gregtech.api.GTValues.V
 import gregtech.api.GTValues.VA
 import gregtech.api.recipes.RecipeMaps.CANNER_RECIPES
-import gregtech.api.recipes.RecipeMaps.CHEMICAL_RECIPES
 import gregtech.api.recipes.RecipeMaps.FLUID_HEATER_RECIPES
 import gregtech.api.recipes.RecipeMaps.MIXER_RECIPES
 import gregtech.api.unification.material.Materials.DistilledWater
-import gregtech.api.unification.material.Materials.Epichlorohydrin
 import gregtech.api.unification.material.Materials.Gelatin
-import gregtech.api.unification.material.Materials.Glycerol
 import gregtech.api.unification.material.Materials.Salt
-import gregtech.api.unification.material.Materials.SeedOil
-import gregtech.api.unification.material.Materials.SodiumHydroxide
-import gregtech.api.unification.material.Materials.Water
 import gregtech.api.unification.ore.OrePrefix.dust
 import gregtechlite.gtlitecore.api.SECOND
 import gregtechlite.gtlitecore.api.TICK
@@ -27,24 +20,16 @@ import gregtechlite.gtlitecore.api.extension.addRecipe
 import gregtechlite.gtlitecore.api.extension.cleanroom
 import gregtechlite.gtlitecore.api.recipe.GTLiteRecipeMaps.LARGE_MIXER_RECIPES
 import gregtechlite.gtlitecore.api.recipe.GTLiteRecipeMaps.SONICATION_RECIPES
-import gregtechlite.gtlitecore.api.recipe.GTLiteRecipeMaps.VACUUM_CHAMBER_RECIPES
 import gregtechlite.gtlitecore.api.unification.GTLiteMaterials.AscorbicAcid
-import gregtechlite.gtlitecore.api.unification.GTLiteMaterials.BeanPhospholipid
 import gregtechlite.gtlitecore.api.unification.GTLiteMaterials.Biotin
 import gregtechlite.gtlitecore.api.unification.GTLiteMaterials.CaneSyrup
-import gregtechlite.gtlitecore.api.unification.GTLiteMaterials.GlycidylStearate
-import gregtechlite.gtlitecore.api.unification.GTLiteMaterials.Monoglyceride
+import gregtechlite.gtlitecore.api.unification.GTLiteMaterials.FoodOilPhaseMixture
 import gregtechlite.gtlitecore.api.unification.GTLiteMaterials.NutrientPasteCrudeEmulsion
 import gregtechlite.gtlitecore.api.unification.GTLiteMaterials.NutrientPasteEmulsion
-import gregtechlite.gtlitecore.api.unification.GTLiteMaterials.FoodOilPhaseMixture
 import gregtechlite.gtlitecore.api.unification.GTLiteMaterials.NutrientPasteWaterPhaseMixture
-import gregtechlite.gtlitecore.api.unification.GTLiteMaterials.OliveOil
 import gregtechlite.gtlitecore.api.unification.GTLiteMaterials.RichNutrientPasteCrudeEmulsion
 import gregtechlite.gtlitecore.api.unification.GTLiteMaterials.RichNutrientPasteEmulsion
 import gregtechlite.gtlitecore.api.unification.GTLiteMaterials.RichNutrientPasteWaterPhaseMixture
-import gregtechlite.gtlitecore.api.unification.GTLiteMaterials.SodiumStearate
-import gregtechlite.gtlitecore.api.unification.GTLiteMaterials.SoybeanOil
-import gregtechlite.gtlitecore.api.unification.GTLiteMaterials.StearicAcid
 import gregtechlite.gtlitecore.api.unification.GTLiteMaterials.SterilizedNutrientPasteEmulsion
 import gregtechlite.gtlitecore.api.unification.GTLiteMaterials.SterilizedRichNutrientPasteEmulsion
 import gregtechlite.gtlitecore.common.item.GTLiteMetaItems.NUTRIENT_PASTE_MEAL
@@ -57,46 +42,6 @@ internal object NutrientPasteMealProcessing
     // @formatter:off
 
     fun init()
-    {
-        monoglycerideProcess()
-        commonProcess()
-    }
-
-    private fun monoglycerideProcess()
-    {
-        // C3H5ClO + C18H35O2Na -> C21H40O3 + NaCl
-        CHEMICAL_RECIPES.addRecipe {
-            fluidInputs(Epichlorohydrin.getFluid(1000))
-            fluidInputs(SodiumStearate.getFluid(1000))
-            output(dust, Salt, 2)
-            fluidOutputs(GlycidylStearate.getFluid(1000))
-            EUt(VA[HV])
-            duration(10 * SECOND)
-        }
-
-        // C21H40O3 + H2O -> C21H42O4
-        CHEMICAL_RECIPES.addRecipe {
-            fluidInputs(GlycidylStearate.getFluid(1000))
-            fluidInputs(Water.getFluid(1000))
-            fluidOutputs(Monoglyceride.getFluid(1000))
-            EUt(VA[LV])
-            duration(5 * SECOND)
-        }
-
-        // Advanced recipe for C21H42O4
-
-        // NaOH + C3H8O3 + C18H36O2 -> C21H42O4 + O (drop)
-        VACUUM_CHAMBER_RECIPES.addRecipe {
-            notConsumable(dust, SodiumHydroxide)
-            fluidInputs(StearicAcid.getFluid(1000))
-            fluidInputs(Glycerol.getFluid(1000))
-            fluidOutputs(Monoglyceride.getFluid(1000))
-            EUt(VA[EV])
-            duration(5 * SECOND)
-        }
-    }
-
-    private fun commonProcess()
     {
         // Nutrient Paste Water Phase Mixture
         MIXER_RECIPES.addRecipe {
@@ -127,19 +72,6 @@ internal object NutrientPasteMealProcessing
             duration(4 * SECOND + 16 * TICK)
             cleanroom()
         }
-
-        // Food Oil Phase Mixture
-        for (oil in arrayOf(SeedOil, OliveOil, SoybeanOil))
-        {
-            LARGE_MIXER_RECIPES.addRecipe {
-                fluidInputs(oil.getFluid(2000))
-                fluidInputs(BeanPhospholipid.getFluid(100))
-                fluidInputs(Monoglyceride.getFluid(100))
-                fluidOutputs(FoodOilPhaseMixture.getFluid(2000))
-                EUt(VA[EV])
-                duration(2 * SECOND + 10 * TICK)
-            }
-        } // TODO: Lecithin.getFluid(50)
 
         // Nutrient Paste Water Phase Mixture + Food Oil Phase Mixture -> Nutrient Paste Crude Emulsion
         MIXER_RECIPES.addRecipe {
