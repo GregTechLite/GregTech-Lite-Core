017892cb600e9e01ba7bc220460aaad479f793f8
Magic_Sweepy <Magic_Sweepy@proton.me>
2026-08-04 12:42:13 +0800

    port common and client proxy to kotlin

---
+++ src/main/kotlin/gregtechlite/gtlitecore/client/ClientProxy.kt
@@ -0,0 +1,58 @@
+package gregtechlite.gtlitecore.client
+
+import gregtechlite.gtlitecore.api.MOD_ID
+import gregtechlite.gtlitecore.client.event.ClientEventHandlers
+import gregtechlite.gtlitecore.client.event.GTLiteTooltips
+import gregtechlite.gtlitecore.client.renderer.handler.tesr.TESRBottlecrate
+import gregtechlite.gtlitecore.client.renderer.texture.GTLiteTextures.preInit
+import gregtechlite.gtlitecore.client.shader.CosmicShaderHelper
+import gregtechlite.gtlitecore.client.shader.CosmicShaderProgram.initShaders
+import gregtechlite.gtlitecore.common.CommonProxy
+import gregtechlite.gtlitecore.common.block.GTLiteBlocks
+import gregtechlite.gtlitecore.common.block.GTLiteBlocks.registerItemModels
+import gregtechlite.gtlitecore.common.entity.GTLiteMetaEntities.initRenderers
+import gregtechlite.gtlitecore.common.tileentity.TileEntityBlockcrate
+import gregtechlite.gtlitecore.core.network.ClientNetworkHandler
+import gregtechlite.gtlitecore.core.network.NetworkHandlerImpl
+import gregtechlite.gtlitecore.core.network.PacketHandler
+import net.minecraftforge.client.event.ModelRegistryEvent
+import net.minecraftforge.common.MinecraftForge
+import net.minecraftforge.fml.client.registry.ClientRegistry
+import net.minecraftforge.fml.common.Mod
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
+import net.minecraftforge.fml.relauncher.Side
+import net.minecraftforge.fml.relauncher.SideOnly
+
+@Mod.EventBusSubscriber(modid = MOD_ID, value = [Side.CLIENT])
+@SideOnly(Side.CLIENT)
+class ClientProxy : CommonProxy()
+{
+
+    override fun onPreInit()
+    {
+        super.onPreInit()
+        preInit()
+        MinecraftForge.EVENT_BUS.register(CosmicShaderHelper)
+        MinecraftForge.EVENT_BUS.register(GTLiteTooltips)
+        MinecraftForge.EVENT_BUS.register(ClientEventHandlers)
+        initShaders()
+        initRenderers()
+        NetworkHandlerImpl.getInstance().registerEventListener(ClientNetworkHandler(PacketHandler.getInstance()))
+    }
+
+    override fun onInit()
+    {
+        super.onInit()
+        GTLiteBlocks.registerColors()
+    }
+
+    companion object
+    {
+        @SubscribeEvent
+        fun registerModels(event: ModelRegistryEvent)
+        {
+            registerItemModels()
+            ClientRegistry.bindTileEntitySpecialRenderer(TileEntityBlockcrate::class.java, TESRBottlecrate())
+        }
+    }
+}
