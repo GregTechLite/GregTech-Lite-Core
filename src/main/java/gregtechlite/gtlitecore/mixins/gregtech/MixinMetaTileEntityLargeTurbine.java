9ac8d63e16cc5e57b4f2d893d8051aecc462a8ae
Magic_Sweepy <Magic_Sweepy@proton.me>
2026-08-25 19:41:22 +0800

    feat: Make Large Turbine can use Item Import Bus to autofill turbine rotor (#164)
    * rotor holder autofill support & allow large turbine use item buses
    
    * add autofill hint tooltip for large turbine

---
+++ src/main/java/gregtechlite/gtlitecore/mixins/gregtech/MixinMetaTileEntityLargeTurbine.java
@@ -0,0 +1,88 @@
+package gregtechlite.gtlitecore.mixins.gregtech;
+
+import gregtech.api.GTValues;
+import gregtech.api.metatileentity.ITieredMetaTileEntity;
+import gregtech.api.metatileentity.MetaTileEntity;
+import gregtech.api.metatileentity.multiblock.FuelMultiblockController;
+import gregtech.api.metatileentity.multiblock.MultiblockAbility;
+import gregtech.api.pattern.BlockPattern;
+import gregtech.api.pattern.FactoryBlockPattern;
+import gregtech.api.recipes.RecipeMap;
+import gregtech.common.metatileentities.multi.electric.generator.MetaTileEntityLargeTurbine;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.resources.I18n;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+
+import java.util.List;
+
+@Mixin(value = MetaTileEntityLargeTurbine.class, remap = false)
+public abstract class MixinMetaTileEntityLargeTurbine extends FuelMultiblockController
+{
+    @Shadow
+    public @Final int tier;
+
+    public MixinMetaTileEntityLargeTurbine(ResourceLocation metaTileEntityId, RecipeMap<?> recipeMap, int tier)
+    {
+        super(metaTileEntityId, recipeMap, tier);
+    }
+
+    /**
+     * @author Magic_Sweepy
+     * @reason Allow all Large Turbines use Item Import Buses for rotor autofill feature.
+     */
+    @Overwrite
+    @Override
+    protected @NotNull BlockPattern createStructurePattern()
+    {
+        return FactoryBlockPattern.start()
+                .aisle("CCCC", "CHHC", "CCCC")
+                .aisle("CHHC", "RGGR", "CHHC")
+                .aisle("CCCC", "CSHC", "CCCC")
+                .where('S', selfPredicate())
+                .where('G', states(getGearBoxState()))
+                .where('C', states(getCasingState()))
+                .where('R', metaTileEntities(MultiblockAbility.REGISTRY.get(MultiblockAbility.ROTOR_HOLDER).stream()
+                        .filter(mte -> (mte instanceof ITieredMetaTileEntity) &&
+                                (((ITieredMetaTileEntity) mte).getTier() >= tier))
+                        .toArray(MetaTileEntity[]::new))
+                        .addTooltips("gregtech.multiblock.pattern.clear_amount_3")
+                        .addTooltip("gregtech.multiblock.pattern.error.limited.1", GTValues.VN[tier])
+                        .setExactLimit(1)
+                        .or(abilities(MultiblockAbility.OUTPUT_ENERGY)).setExactLimit(1))
+                .where('H', states(getCasingState())
+                        .or(autoAbilities(false, true, false, false, true, true, true))
+                        // region patch
+                        .or(abilities(MultiblockAbility.IMPORT_ITEMS)
+                                .setPreviewCount(0)))
+                        // endregion
+                .build();
+    }
+
+    /**
+     * @author Magic_Sweepy
+     * @reason Add autofill support hint for player.
+     */
+    @Overwrite
+    @Override
+    public void addInformation(ItemStack stack, @Nullable World world, @NotNull List<String> tooltip, boolean advanced)
+    {
+        super.addInformation(stack, world, tooltip, advanced);
+        tooltip.add(I18n.format("gregtech.universal.tooltip.base_production_eut", GTValues.V[tier] * 2));
+        tooltip.add(I18n.format("gregtech.multiblock.turbine.efficiency_tooltip", GTValues.VNF[tier]));
+        tooltip.add(I18n.format("gtlitecore.machine.large_turbine.autofill"));
+    }
+
+    @Shadow
+    public abstract IBlockState getCasingState();
+
+    @Shadow
+    public abstract IBlockState getGearBoxState();
+}
