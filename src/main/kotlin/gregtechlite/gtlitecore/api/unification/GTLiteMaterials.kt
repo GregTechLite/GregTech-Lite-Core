035740e5e1fcc98b8ac9a0c60e0afd57a1cc1e7a
Magic_Sweepy <Magic_Sweepy@proton.me>
2026-09-14 11:26:13 +0800

    add pigment red for magenta dye

--- src/main/kotlin/gregtechlite/gtlitecore/api/unification/GTLiteMaterials.kt
+++ src/main/kotlin/gregtechlite/gtlitecore/api/unification/GTLiteMaterials.kt
@@ -503,6 +503,7 @@ object GTLiteMaterials
     // endregion
 
     // region Organic Chemistry Materials (id: 8001-12000)
+
     lateinit var Dicyclopentadiene: Material
     lateinit var Pentane: Material
     lateinit var Polyisoprene: Material
@@ -750,6 +751,7 @@ object GTLiteMaterials
     lateinit var Dimethylketene: Material
     lateinit var Tetramethylcyclobutanediol: Material
     lateinit var CBDOPolycarbonate: Material
+    lateinit var PigmentRed: Material
 
     // endregion
 
