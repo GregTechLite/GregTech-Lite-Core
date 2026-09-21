728e49451a7217fb47921b77c95649505c328db9
Magic_Sweepy <Magic_Sweepy@proton.me>
2026-09-21 10:58:00 +0800

    reformat tooltips for quantum storage units

--- src/main/kotlin/gregtechlite/gtlitecore/api/extension/NumberExt.kt
+++ src/main/kotlin/gregtechlite/gtlitecore/api/extension/NumberExt.kt
@@ -5,3 +5,19 @@ import java.math.BigInteger
 fun Int.square(): Int = this * this
 
 fun BigInteger.longValue(): Long = min(BigInteger.valueOf(Long.MAX_VALUE)).longValueExact()
+
+fun BigInteger.formatting(): String
+{
+    // CEu why you not impl it in SmallDigits? :(
+    val superscripts = mapOf('0' to '⁰', '1' to '¹', '2' to '²', '3' to '³', '4' to '⁴', '5' to '⁵', '6' to '⁶',
+                             '7' to '⁷', '8' to '⁸', '9' to '⁹', '-' to '⁻', '+' to '⁺')
+
+    fun Int.toSuperscript(): String = toString().map { superscripts[it] ?: it }.joinToString("")
+
+    if (signum() == 0) return "0"
+
+    val decimal = toBigDecimal()
+    val exp = decimal.precision() - decimal.scale() - 1
+
+    return "10${exp.toSuperscript()}"
+}
