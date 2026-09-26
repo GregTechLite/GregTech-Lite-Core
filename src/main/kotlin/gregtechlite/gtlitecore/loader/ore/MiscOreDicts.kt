a8f823885347eb7fed09fe0b00b43467b9331566
Magic_Sweepy <Magic_Sweepy@proton.me>
2026-09-26 18:16:35 +0800

    fix sapling tree meta missing for oredict register

--- src/main/kotlin/gregtechlite/gtlitecore/loader/ore/MiscOreDicts.kt
+++ src/main/kotlin/gregtechlite/gtlitecore/loader/ore/MiscOreDicts.kt
@@ -1,28 +1,17 @@
 package gregtechlite.gtlitecore.loader.ore
 
-import gregtechlite.gtlitecore.common.block.GTLiteBlocks
+import gregtechlite.gtlitecore.api.extension.toItem
+import gregtechlite.gtlitecore.common.worldgen.generator.tree.WorldGeneratorTreeRegistry
 import net.minecraftforge.oredict.OreDictionary
 
-@Suppress("Deprecation")
 internal object MiscOreDicts
 {
-
     // @formatter:off
 
     fun init()
     {
-        OreDictionary.registerOre("treeSapling", GTLiteBlocks.SAPLINGS[0].getStateFromMeta(0).block)
-        OreDictionary.registerOre("treeSapling", GTLiteBlocks.SAPLINGS[0].getStateFromMeta(2).block)
-        OreDictionary.registerOre("treeSapling", GTLiteBlocks.SAPLINGS[0].getStateFromMeta(4).block)
-        OreDictionary.registerOre("treeSapling", GTLiteBlocks.SAPLINGS[0].getStateFromMeta(6).block)
-        OreDictionary.registerOre("treeSapling", GTLiteBlocks.SAPLINGS[0].getStateFromMeta(8).block)
-        OreDictionary.registerOre("treeSapling", GTLiteBlocks.SAPLINGS[0].getStateFromMeta(10).block)
-        OreDictionary.registerOre("treeSapling", GTLiteBlocks.SAPLINGS[0].getStateFromMeta(12).block)
-        OreDictionary.registerOre("treeSapling", GTLiteBlocks.SAPLINGS[0].getStateFromMeta(14).block)
-        OreDictionary.registerOre("treeSapling", GTLiteBlocks.SAPLINGS[1].getStateFromMeta(0).block)
-        OreDictionary.registerOre("treeSapling", GTLiteBlocks.SAPLINGS[1].getStateFromMeta(2).block)
+        WorldGeneratorTreeRegistry.forEach { OreDictionary.registerOre("treeSapling", it.saplingState.toItem()) }
     }
 
     // @formatter:on
-
 }
